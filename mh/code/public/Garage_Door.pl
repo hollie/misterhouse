f54237766e64e4e381253d6117361456bb98f436
danal <danal@71d350d0-f70e-0410-9bbe-9702f904aa17>
2000-02-12 00:11:02 +0000

    Clean up startup states; Added notification if open too long.

--- mh/code/public/Garage_Door.pl
+++ mh/code/public/Garage_Door.pl
@@ -11,8 +11,9 @@
 #                                                                #
 ##################################################################
 
-use vars '$door1_old', '$door2_old', '$door3_old';
+use vars '$door1_old', '$door2_old', '$door3_old', '$warning_sent';
 $garage_doors = new X10_Garage_Door('D');  # CHANGE THIS to housecode of your RF Vehicle Link
+$timer_garage_door = new  Timer();
 
 # Returned state is "bbbdccc"
 # "bbb" is 1=door enrolled, 0=enrolled, indexed by door # (i.e. 123)
@@ -62,6 +63,13 @@ if (state_now $garage_doors) {
       }
    }
 
+    set $timer_garage_door 300 if substr($state,4,3) ne "CCC"; # Start (or push out) a timer if anything is open
+
+    if ($warning_sent and (substr($state,4,3) eq "CCC") ) {
+      &garage_notify("Cancel Warning: Garage doors all CLOSED");
+      $warning_sent = 0;
+   }
+
    $door1_old = $door1;
    $door2_old = $door2;
    $door3_old = $door3;
@@ -70,10 +78,10 @@ if (state_now $garage_doors) {
 
 # Prove we can query garage door data asynchronously
 
-$test_button = new X10_Item('B4', 'on');
-if (state_now $test_button) {
+$Garage_Query_Button = new X10_Item('B1');
+if (state_now $Garage_Query_Button) {
 
-   print "\n\n Button B4 pushed - Garage Status Query\n";
+   print "\n\n Button B1 pushed - Garage Status Query\n";
    my $state = state $garage_doors;
    my ($en1, $en2, $en3, $which, $door1, $door2, $door3) = $state =~ /(\S)(\S)(\S)(\S)(\S)(\S)(\S)/;
    my %table_dcode = qw(O Open C Closed);
@@ -85,4 +93,46 @@ if (state_now $test_button) {
    print "Door 3 state is $table_dcode{$door3}\n" if $en3;
    print "\n";
  
+}
+
+# Test mail notification...
+
+$Garage_Mail_Test = new X10_Item('B2');
+if (state_now $Garage_Mail_Test) {
+   print "\n\n Button B2 pushed - Garage Mail Test\n";
+   my $state = state $garage_doors;
+   $state = substr($state,4,3);
+   &garage_notify("Warning, Garage doors test warning");
+}
+
+# Notify Danal if garage door left open
+
+if (expired $timer_garage_door) {
+   my $state = state $garage_doors;
+   $state = substr($state,4,3);
+   if ($state ne "CCC") {
+      &garage_notify("Warning, Garage doors open too long");
+      $warning_sent = 1;
+   }  
+}
+
+# Subroutine to send a page / pcs message, etc.
+sub garage_notify {
+   my ($text) =@_;
+
+   my $p1 = new Process_Item("send_sprint_pcs -to danal -text \"$text $Date_Now $Time_Now\" ");
+   start $p1;      # Run externally so as not to hang MH process
+   my $p2 = new Process_Item("alpha_page -pin 1488774 -message \"$text $Date_Now $Time_Now\" ");
+   start $p2;      # Run externally so as not to hang MH process
+
+   speak $text;
+}
+
+# Various maintenance / startup stuff
+if (($Startup) or
+    ($Reload)) {
+   $door1_old = 'C';
+   $door2_old = 'C';
+   $door3_old = 'C';
+   $warning_sent = 0;
 }
