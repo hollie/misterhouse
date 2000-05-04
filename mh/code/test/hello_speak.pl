cc7d8eede7dae36c67c127c90f6c01f39dcd4799
cwitte <cwitte@71d350d0-f70e-0410-9bbe-9702f904aa17>
2000-05-04 13:25:01 +0000

    added incorrecly.  deleting to prep for re-add.

--- mh/code/test/hello_speak.pl
+++
@@ -1,9 +0,0 @@
-# Category=Test
-
-
-				# speak time if asked for, and every minute
-$v_tell_time = new  Voice_Cmd('What is the time');
-if (said $v_tell_time or $New_Minute) {
-    print_log "It is $Time_Now on $Date_Now.";
-    speak("It is $Time_Now on $Date_Now.");
-}
--- mh/code/test/test_tagline.pl
+++
@@ -1,5 +0,0 @@
-# Category=Test
-
-$house_tagline = new  File_Item("$Pgm_Root/data/remarks/1100tags.txt");
-speak(read_random $house_tagline) if $New_Minute;
-
