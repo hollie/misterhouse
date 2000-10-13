c11f44c1c3552e13f2eea45b22a29e55f3bb22e9
danal <danal@71d350d0-f70e-0410-9bbe-9702f904aa17>
2000-10-13 20:21:57 +0000

    Misc tweaks

--- mh/code/public/Danal/Cat_Box.pl
+++ mh/code/public/Danal/Cat_Box.pl
@@ -8,36 +8,40 @@
 #                                                                #
 ##################################################################
 
-$Cat_Box_Control  = new Serial_Item('XC4CJ');
-$Cat_Box_Control -> add            ('XC4CK');
-$Cat_Box_Control -> add            ('XC5CK');
-$Cat_Box_Control -> add            ('XC5CJ');
-$Cat_Box_Control -> add            ('XC6CK');
-$Cat_Box_Control -> add            ('XC6CJ');
+$Cat_Box_Control  = new Serial_Item('XC4CJ','Scoop');
+$Cat_Box_Control -> add            ('XC4CK','Full');
+$Cat_Box_Control -> add            ('XC5CK','5on');
+$Cat_Box_Control -> add            ('XC5CJ','5off');
+$Cat_Box_Control -> add            ('XC6CK','6on');
+$Cat_Box_Control -> add            ('XC6CJ','6off');
+
+$Cat_Box_Reminder = new X10_Item('JG');
 
 if (state_now $Cat_Box_Control) {
    my $state = state $Cat_Box_Control;
-   if ($state eq 'XC4CJ') { 
+   if ($state eq 'Scoop') { 
       print_log "Cat Box Button 4 pushed ON - Cat Box Scooped for the day";
+      set $Cat_Box_Reminder OFF;
    }
-   if ($state eq 'XC4CK') { 
+   if ($state eq 'Full') { 
       print_log "Cat Box Button 4 pushed OFF - Cat Box full change";
+      set $Cat_Box_Reminder OFF;
    }
-   if ($state eq 'XC5CJ') {
+   if ($state eq '5on') {
       print_log "Cat Box Button 5 pushed ON";
    }
-   if ($state eq 'XC5CK') {
+   if ($state eq '5off') {
       print_log "Cat Box Button 5 pushed OFF";
    }
-   if ($state eq 'XC6CJ') {
+   if ($state eq '6on') {
       print_log "Cat Box Button 6 pushed ON";
    }
-   if ($state eq 'XC6CK') {
+   if ($state eq '6off') {
       print_log "Cat Box Button 6 pushed OFF";
   }
 }
 
-if ($New_Minute and time_cron '1,16,31,46 18,19,20,21,22,23 * * *') {
+if ($New_Minute and time_cron '1,16,31,46 12,13,14,15,16,17,18,19,20,21,22,23 * * *') {
    my ($date, $time, $state) = (state_log $Cat_Box_Control)[0] =~ /(\S+) (\S+) *(.*)/;
    use Time::ParseDate;
    my $tnow=parsedate('now');
@@ -45,9 +49,16 @@ if ($New_Minute and time_cron '1,16,31,46 18,19,20,21,22,23 * * *') {
    my $tdiff = $tnow - $tcat;
 print_log "Cat Box Timers: Last date/time $date $time, seconds $tcat, secs now $tnow, diff $tdiff";
 
-   if ($tdiff > 24*60*60) {
-      print_log "Cat box not changed in 24 hours; speaking nag message";
-	speak "Meeeoow, Change the cat box";
+   if ($tdiff > 18*60*60) {
+      print_log "Cat box not changed today; speaking nag message";
+	speak "Djeeni says: Meeeoow, Change the cat box";
+      set $Cat_Box_Reminder ON if 'on' ne state $Cat_Box_Reminder;
+   }
+   if ($tdiff > 42*60*60) {
+	speak "Really!";
+   }
+   if ($tdiff > 64*60*60) {
+	speak "It Stinks!";
    }
 }
 
--- mh/code/public/Danal/Garage_Door.pl
+++ mh/code/public/Danal/Garage_Door.pl
@@ -12,8 +12,11 @@
 ##################################################################
 
 use vars '$door1_old', '$door2_old', '$door3_old', '$warning_sent';
+
 $garage_doors = new X10_Garage_Door('D');  # CHANGE THIS to housecode of your RF Vehicle Link
 $timer_garage_door = new  Timer();
+$garage_motion = new X10_Appliance('DF');
+$garage_dusk   = new X10_Appliance('DG');
 
 # Returned state is "bbbdccc"
 # "bbb" is 1=door enrolled, 0=enrolled, indexed by door # (i.e. 123)
@@ -27,39 +30,39 @@ if (state_now $garage_doors) {
 
    my $debug = 1 if $config_parms{debug} eq 'garage';
    if ($debug) {
-   print "\n\n   Garage Door Transmission Received\n";
-   print "State=$state\n";
-   print "Transmission from $which\n";
-   print "Door 1 state is $table_dcode{$door1}\n" if $en1;
-   print "Door 2 state is $table_dcode{$door2}\n" if $en2;
-   print "Door 3 state is $table_dcode{$door3}\n" if $en3;
-   print "\n";
+   print_log "\n\n   Garage Door Transmission Received\n";
+   print_log "State=$state\n";
+   print_log "Transmission from $which\n";
+   print_log "Door 1 state is $table_dcode{$door1}\n" if $en1;
+   print_log "Door 2 state is $table_dcode{$door2}\n" if $en2;
+   print_log "Door 3 state is $table_dcode{$door3}\n" if $en3;
+   print_log "\n";
    }
 
    if ($which eq '1') {
       if ($door1 eq $door1_old) {
-         print "Door 1 timer or retransmit update, door1 $table_dcode{$door1}\n" if $debug;
+         print_log "Door 1 timer or retransmit update, door1 $table_dcode{$door1}\n" if $debug;
       }  else {
-         print "Door 1 status change, old $table_dcode{$door1_old}, new $table_dcode{$door1}\n";
-         speak "Door 1 is $table_dcode{$door1}";
+         print_log "Door 1 status change, old $table_dcode{$door1_old}, new $table_dcode{$door1}\n";
+         speak "Djeeni says: Door 1 is $table_dcode{$door1}";
       }
    }
 
    if ($which eq '2') {
       if ($door2 eq $door2_old) {
-         print "Door 2 timer or retransmit update, door2 $table_dcode{$door2}\n" if $debug;
+         print_log "Door 2 timer or retransmit update, door2 $table_dcode{$door2}\n" if $debug;
       }  else {
-         print "Door 2 status change, old $table_dcode{$door2_old}, new $table_dcode{$door2}\n";
-         speak "Door 2 is $table_dcode{$door2}";
+         print_log "Door 2 status change, old $table_dcode{$door2_old}, new $table_dcode{$door2}\n";
+         speak "Djeeni says: Door 2 is $table_dcode{$door2}";
       }
    }
 
    if ($which eq '3') {
       if ($door3 eq $door3_old) {
-         print "Door 3 timer or retransmit update, door3 $table_dcode{$door3}\n" if $debug;
+         print_log "Door 3 timer or retransmit update, door3 $table_dcode{$door3}\n" if $debug;
       }  else {
-         print "Door 3 status change, old $table_dcode{$door3_old}, new $table_dcode{$door3}\n";
-         speak "Door 3 is $table_dcode{$door3}";
+         print_log "Door 3 status change, old $table_dcode{$door3_old}, new $table_dcode{$door3}\n";
+         speak "Djeeni says: Door 3 is $table_dcode{$door3}";
       }
    }
 
@@ -75,34 +78,15 @@ if (state_now $garage_doors) {
    $door3_old = $door3;
 }
 
+# Check for motion in garage
 
-# Prove we can query garage door data asynchronously
-
-$Garage_Query_Button = new X10_Item('B1');
-if (state_now $Garage_Query_Button) {
-
-   print "\n\n Button B1 pushed - Garage Status Query\n";
-   my $state = state $garage_doors;
-   my ($en1, $en2, $en3, $which, $door1, $door2, $door3) = $state =~ /(\S)(\S)(\S)(\S)(\S)(\S)(\S)/;
-   my %table_dcode = qw(O Open C Closed);
-
-   print "State=$state\n";
-   print "Last transmission from $which\n";
-   print "Door 1 state is $table_dcode{$door1}\n" if $en1;
-   print "Door 2 state is $table_dcode{$door2}\n" if $en2;
-   print "Door 3 state is $table_dcode{$door3}\n" if $en3;
-   print "\n";
- 
-}
-
-# Test mail notification...
-
-$Garage_Mail_Test = new X10_Item('B2');
-if (state_now $Garage_Mail_Test) {
-   print "\n\n Button B2 pushed - Garage Mail Test\n";
-   my $state = state $garage_doors;
-   $state = substr($state,4,3);
-   &garage_notify("Warning, Garage doors test warning");
+if (state_now $garage_motion) {
+  if ('on' eq state $garage_motion) {
+    if (active $timer_garage_door) { 
+      # set $timer_garage_door 300; 
+      print_log "Garage test: Motion ON detect in garage w/timer active.";
+    }
+  }
 }
 
 # Notify Danal if garage door left open
@@ -111,9 +95,37 @@ if (expired $timer_garage_door) {
    my $state = state $garage_doors;
    $state = substr($state,4,3);
    if ($state ne "CCC") {
-      &garage_notify("Warning, Garage doors open too long");
+      &garage_notify("Warning, Garage doors open too long $state");
       $warning_sent = 1;
    }  
+}
+
+
+# Prove we can query garage door data asynchronously
+
+$v_Garage_Query = new Voice_Cmd('Query Garage Door', 0);
+$v_Garage_Query-> set_info('Print log of garage door status info');
+
+if (said $v_Garage_Query) {
+   my $state = state $garage_doors;
+   my ($en1, $en2, $en3, $which, $door1, $door2, $door3) = $state =~ /(\S)(\S)(\S)(\S)(\S)(\S)(\S)/;
+   my %table_dcode = qw(O Open C Closed);
+
+   print_log "State=$state\n";
+   print_log "Last transmission from $which\n";
+   print_log "Door 1 state is $table_dcode{$door1}\n" if $en1;
+   print_log "Door 2 state is $table_dcode{$door2}\n" if $en2;
+   print_log "Door 3 state is $table_dcode{$door3}\n" if $en3;
+}
+
+# Test mail notification...
+
+$v_Garage_Mail_Test = new Voice_Cmd('Garage Door Mail Test', 0);
+
+if (said $v_Garage_Mail_Test) {
+   my $state = state $garage_doors;
+   $state = substr($state,4,3);
+   &garage_notify("Warning, Garage doors test warning $state");
 }
 
 # Subroutine to send a page / pcs message, etc.
@@ -122,10 +134,11 @@ sub garage_notify {
 
    my $p1 = new Process_Item("send_sprint_pcs -to danal -text \"$text $Date_Now $Time_Now\" ");
    start $p1;      # Run externally so as not to hang MH process
-   my $p2 = new Process_Item("alpha_page -pin 1488774 -message \"$text $Date_Now $Time_Now\" ");
+   my $p2 = new Process_Item("alpha_page -pin 9999999 -message \"$text $Date_Now $Time_Now\" ");
    start $p2;      # Run externally so as not to hang MH process
 
-   speak $text;
+   print_log "Garage notification sent, text = $text";
+   speak "Djeeni says: $text";
 }
 
 # Various maintenance / startup stuff
