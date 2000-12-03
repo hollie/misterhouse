553b69a02977e149df8f9e333d5e11ab5d070a14
(no author) <(no author)@71d350d0-f70e-0410-9bbe-9702f904aa17>
2000-12-03 19:59:48 +0000

    This commit was manufactured by cvs2svn to create tag 'v2-36'.

--- mh/code/public/weather_vw.pl
+++
@@ -1,9 +0,0 @@
-
-# Category=Weather
-
-# Add these 2 mh.ini parms
-#   weather_vwlog_file=c:\vweather\log_file
-#   weather_vwlog_module=Weather_vw.pm
-
-$WindSpeed = new Weather_Item 'WindAvgSpeed';
-$WindSpeed-> tie_event('print_log "VW Weather wind speed is now at $state"');
