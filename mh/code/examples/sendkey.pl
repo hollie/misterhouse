f447e3a0fcff8f8fcc4b0751857ababc51766b49
winter <winter@71d350d0-f70e-0410-9bbe-9702f904aa17>
2000-12-21 20:14:12 +0000

    - 2.39 release

---
+++ mh/code/examples/sendkey.pl
@@ -0,0 +1,15 @@
+
+# Sendkeys is a windows only (currently not in perl 5.6) way
+# of controling other windows
+
+$test_sendkey = new Voice_Cmd 'Test sendkey [1,2,3]';
+
+if ($state = said $test_sendkey) {
+                                # Send data to notepad
+    if (my $window = &sendkeys_find_window('Notepad', 'notepad')) {
+        my $keystr = '33';
+        &SendKeys($window, $keystr, 1, 500) if $state == 1;
+        &SendKeys($window, '33',    1, 500) if $state == 2;
+        print_log "keys sent: $state";
+    }
+}
---
+++ mh/code/examples/sendkey_browser.pl
@@ -0,0 +1,21 @@
+
+# On windows, use sendkeys to point a browser to a url
+#  - does not work too well, due to timing issues for
+#    the open window
+
+$browser_test = new Voice_Cmd 'Test browser [1,2,3]';
+
+if ($state = said $browser_test) {
+    print_log "Running browser test $state";
+    if ($state == 1) {
+        if (my $window = &sendkeys_find_window('Explorer', 'C:\Progra~1\Intern~1\IEXPLORE.EXE')) {
+            my $keys = '\\ctrl\\o\\ctrl-\\';
+            &SendKeys($window, $keys, 1);
+        }
+        sleep .4;
+        if (my $window = &sendkeys_find_window('Open')) {
+            my $keys = 'http://misterhouse.net\\ret\\';
+            &SendKeys($window, $keys, 1);
+        }	
+    }
+}
