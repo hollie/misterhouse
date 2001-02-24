63f44ce046a8b44c7caf2bb0fe276367138d6694
winter <winter@71d350d0-f70e-0410-9bbe-9702f904aa17>
2001-02-24 23:18:40 +0000

    - 2.45 release

--- mh/bin/mh.bat
+++ mh/bin/mh.bat
@@ -1,12 +1,54 @@
 @echo off
 
-@rem Should not need to set this
-@rem set perl5lib=\mh\lib;\mh\lib\site;\mh\bin
+@rem This is a dos mh loop for restarting mh if mh had an unexpected exit.
+@rem This will call mh.exe (if it exists) or 'perl mh' otherwise.
+@rem It checks exit codes so it can loop if a non-requested exit occured
+@rem Note:  This must be run from the mh\bin directory
 
+@rem Gather up arguments into one var
 set pgmargs=
-:getargs
+set noloop=0
+:GETARGS
 set pgmargs=%pgmargs% %1
+if '%1' == '-run' set noloop=1
 shift
-if not '%1'=='' goto getargs
+if not '%1' == '' goto GETARGS
+if %noloop% == 1  goto START
 
+:RERUN
+@rem This file will be created if mh starts normally (i.e. does not die on startup)
+del mh.started
+
+:START
+if EXIST mh.exe goto COMPILED
+
+echo Starting interpreted perl mh
 perl -S mh %pgmargs%
+@rem This test means exit=1 (normal exit) and not > 1
+@rem if errorlevel 1 if not errorlevel 2 goto DONE
+if %errorlevel% == 1 goto DONE
+if %noloop%     == 1 goto DONE
+goto FAIL
+
+:COMPILED
+echo Starting compiled mh.exe
+mh.exe %pgmargs%
+if %errorlevel% == 1 goto DONE
+if %noloop%     == 1 goto DONE
+
+:FAIL
+
+if not EXIST mh.started goto DONE2
+
+echo. | date >> mh_restart.log
+echo accidental exit on mh, restarting ... >> mh_restart.log
+echo accidental exit on mh, restarting ...
+@rem sleep 5
+goto RERUN
+
+:DONE2
+echo mh had an error on startup, will not restart
+
+:DONE
+@rem echo User exit of mh completed
+
