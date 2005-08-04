ee0d2caf060dd4664c5d0e2b56e920f720614bb5
mattrwilliams <mattrwilliams@71d350d0-f70e-0410-9bbe-9702f904aa17>
2005-08-04 13:09:54 +0000

    Added support for printing via temporary file for Windows environments. Changed mail and plain text print output to be multiple items per row.

--- mh/web/bin/shopping_list.pl
+++ mh/web/bin/shopping_list.pl
@@ -1,5 +1,5 @@
 # Shopping List
-# Version 1.2
+# Version 1.5
 # Matthew Williams
 #
 # This file should be placed in mh/web/bin.  I link to it through a my_mh page.
@@ -13,20 +13,44 @@
 # [Desserts]
 # Cake=0
 #
+# If shopping_print_type is 'pipe':
 # shopping_print will be fed an HTML file suitable for printing
 # I suggest using html2ps to convert the html to postscript and then
 # piping the output to lp
 # e.g.
+# shopping_print_type=pipe
 # shopping_print=/usr/local/bin/html2ps | /usr/bin/lp
 #
+# If shopping_print_type is 'file':
+# shopping print will be run with any occurance of _FILE_ replaced
+# with a filename containing the shopping list in plain text
+# e.g.
+# shopping_print_type=file
+# shopping_print=prfile32 _FILE_
+#
+# In the above example, prfile32 is assumed to be somewhere on your PATH.
+# If not, supply a path to the executable and not just the executable.
+#
+# FYI, PrintFile http://www.lerup.com/printfile is a good way of
+# printing text files from the Windows command line 
+#
 # shopping_columns determines how many items are displayed per column
-# This is useful to tuning the output to match your screen resolution
+# This is useful for tuning the output to match your screen resolution
 #
 # shopping_email determines which e-mail address will receive the 
 # shopping list.  If blank, will default to net_mail_account_address
 #
 # Revision History:
 # 
+# Version 1.5: Matthew Williams
+# - changed output of plain text files to be columns
+#
+# Version 1.4: Matthew Williams
+# - added print_file_type to better support Windows environments
+#
+# Version 1.3: Matthew Williams
+# - added "at store" button and related functionality
+#
 # Version 1.2: Matthew Williams
 # - added section headers to printed output
 # - added ability to send list to an e-mail address
@@ -41,6 +65,9 @@
 my $file=$config_parms{shopping_list};
 $file = "$Pgm_Root/data/shopping_list.txt" unless $file and -e $file;
 my $printCommand=$config_parms{shopping_print};
+my $printCommandType=$config_parms{shopping_print_type};
+$printCommandType='pipe' unless $printCommandType eq 'file';
+my $printOutput=$file.'.output';
 my $tempFile=$file.'.temp';
 my $action;
 my $sectionHeader;
@@ -76,7 +103,7 @@ sub shoppingListError {
 $param{'action'}='list' unless defined($param{'action'});
 $param{'action'}='list' if $param{'action'} eq 'cancel';
 my $printing=(($param{'action'} eq 'print') or ($param{'action'} eq 'print preview') or ($param{'action'} eq 'e-mail'));
-
+my $atShop=(($param{'action'} eq 'at shop') or ($param{'action'} eq 'remove items'));
 
 $html=qq[
 <html>
@@ -100,6 +127,7 @@ h4 { font-family: helvetica;
 <body>
 <form name="main">
 ];
+
 
 if ($param{'action'} eq 'add item') {
 	if (($param{'category'} eq 'select') or ($param{'item'} eq '')) {
@@ -178,7 +206,7 @@ if ($param{'action'} eq 'add item') {
 	return $html;
 }
 
-if (($param{'action'} eq 'update list') or ($param{'action'} eq 'clear all')) {
+if (($param{'action'} eq 'update list') or ($param{'action'} eq 'clear all') or ($param{'action'} eq 'remove items')) {
 	open (OLDLIST,$file) || return shoppingListError("$file: $!");
 	open (NEWLIST,">$tempFile") || return shoppingListError("$tempFile: $!");
 	while (<OLDLIST>) {
@@ -192,6 +220,9 @@ if (($param{'action'} eq 'update list') or ($param{'action'} eq 'clear all')) {
 			if ($param{'action'} eq 'update list') {
 				$newVal=$param{$1} eq 'on' ? '1' : '0';
 			}
+			if ($param{'action'} eq 'remove items') {
+				$newVal=$param{$1} eq 'on' ? '0' : $2;
+			}
 			print NEWLIST "$1=".$newVal."\n"; next;};
 		}
 	close OLDLIST;
@@ -200,10 +231,12 @@ if (($param{'action'} eq 'update list') or ($param{'action'} eq 'clear all')) {
 	if ($param{'action'} eq 'clear all') {
 		$html.="<h3>All Items Cleared</h3>\n";
 	}
-	$html.= "<h3>List Updated</h3>\n";
+	if (not $atShop) {
+		$html.= "<h3>List Updated</h3>\n";
+	}
 }
 
-if ( $printing) {
+if ($printing) {
 	$html.=qq[<h2>Shopping List</h2>];
 	if ($param{'action'} eq 'print preview') {
 		$html.=qq[<h4>Preview Only<h4>];
@@ -216,6 +249,16 @@ if ( $printing) {
 
 open (SHOPLIST, $file) || return shoppingListError("$file: $!");
 my $num=0;
+
+my $showAll=1;
+
+$showAll=0 if $atShop==1;
+
+if ($atShop) {
+	$html.=qq[<h2>Shopping List At Shop</h2>];
+	$html.=qq[<input type=submit name="action" value="cancel">];
+}
+
 while (<SHOPLIST>)
 	{
 	chomp;
@@ -225,17 +268,23 @@ while (<SHOPLIST>)
 	/^\[(.+)\]$/ && do {
 		if ($printing) {
 			$sectionHeader="\n</table><h3>$1</h3><table>\n\n";
-		} else {
-		$html.=qq[</table><input type=submit name="action" value="update list">\n];
-		$html.=qq[<input type=submit name="action" value="print preview">\n];
-		$html.=qq[<input type=submit name="action" value="print">\n];
-		$html.=qq[<input type=submit name="action" value="e-mail">\n];
-		$html.=qq[<input type=submit name="action" value="add item">\n];
-		$html.=qq[<input type=submit name="action" value="cancel">\n];
-		$html.=qq[<input type=submit name="action" value="clear all"></p>\n];
+		} else {	
+			if ($atShop) {
+				$sectionHeader=qq[</table><hr><h3>$1</h3><input type=submit name="action" value="remove items">\n];
+				$sectionHeader.=qq[<input type=submit name="action" value="cancel"><table></p>\n];
+			} else {
+				$html.=qq[</table><input type=submit name="action" value="update list">\n];
+				$html.=qq[<input type=submit name="action" value="print preview">\n];
+				$html.=qq[<input type=submit name="action" value="print">\n];
+				$html.=qq[<input type=submit name="action" value="e-mail">\n];
+				$html.=qq[<input type=submit name="action" value="add item">\n];
+				$html.=qq[<input type=submit name="action" value="at shop">\n];
+				$html.=qq[<input type=submit name="action" value="cancel">\n];
+				$html.=qq[<input type=submit name="action" value="clear all"></p>\n];
 			$html.=qq[<hr><h3>$1</h3><table width="100%"><colgroup span="$numColumns" width="$columnWidth"><thead>\n];
-			$num=0;
 			}
+		$num=0;
+		}
 		next;
 	};
 	/^(.+)=(.+)$/ && do {
@@ -251,13 +300,26 @@ while (<SHOPLIST>)
 				$html.=qq[<td><input type=checkbox>$item\n];
 			}
 		} else {
+			my $checked;
+			if ($atShop) {
+				if ($value==0) {
+					next;
+				}
+				$html.=$sectionHeader;
+				$sectionHeader='';
+				$checked=0;
+			} else {
+				$checked=$value;
+			}
+	
 			if ($num % $numColumns == 0) {
 				$html.='<tr>';
 			}
 			$num++;
 			my $fieldname=$item;
 			$fieldname =~ s/ /%20;/g;
-			$html.=qq[<td width="$columnWidth"><input type=checkbox name="$item"]. ($value ? ' checked' : '') . '>';
+
+			$html.=qq[<td width="$columnWidth"><input type=checkbox name="$item"]. ($checked ? ' checked' : '') . '>';
 			$html.=qq[<a onClick="document.main.elements['$item'].checked=!document.main.elements['$item'].checked">$item</a>\n];
 		}
 		next;
@@ -270,24 +332,41 @@ if (!$printing) {
 }
 
 $html.='</body></html>';
+my $plainText=$html;
+
+# Strip away HTML stuff from mailtext
+$plainText =~ s/<h2.+?<\/h2>//gs;
+$plainText =~ s/<h3>(.*?)<\/h3>/\n\*\* $1 \*\*/sg;
+$plainText =~ s/<title.+?<\/title>//gs;
+$plainText =~ s/<style.+?<\/style>//gs;
+$plainText =~ s/<tr.*?>/\n/gs;
+$plainText =~ s/(<td.+?)\n/$1   /gs;
+$plainText =~ s/<.+?>//g;
+$plainText =~ s/E-Mail List//g;
+$plainText =~ s/^\n+//s;
+$plainText =~ s/\n{3,}/\n\n/gs;
+$plainText .= "\n";
+
 if ($param{'action'} eq 'print') {
-	open (PRINTER, "| $printCommand") || return shoppingListError("$printCommand: $!");
-	print PRINTER $html;
-	close (PRINTER);
+	if ($printCommandType eq 'pipe') {
+		open (PRINTER, "| $printCommand") || return shoppingListError("$printCommand: $!");
+		print PRINTER $html;
+		close (PRINTER);
+	} elsif ($printCommandType eq 'file') {
+		open (PRINTER, "> $printOutput") || return shoppingListError("$printOutput: $!");
+		print PRINTER $plainText;
+		close PRINTER;
+		$printCommand =~ s/_FILE_/$printOutput/g;
+		system ($printCommand);
+	}
 }
 
 if ($param {'action'} eq 'e-mail') {
   if (&net_connect_check) {
     my $mailtext=$html;
-    $mailtext =~ s/<h2.+?<\/h2>//gs;
-    $mailtext =~ s/<h3>(.*?)<\/h3>/\*\* $1 \*\*/sg;
-    $mailtext =~ s/<title.+?<\/title>//gs;
-    $mailtext =~ s/<style.+?<\/style>//gs;
-    $mailtext =~ s/<.+?>//g;
-    $mailtext =~ s/E-Mail List//g;
-    $mailtext =~ s/^\n+//s;
+
     &net_mail_send(subject => 'shopping list', 
-                   text => $mailtext,
+                   text => $plainText,
                    to => $config_parms{shopping_email}
     );
   } else {
