95a100a0358ecdd6122df7ff8457e9e8edd6b690
mattrwilliams <mattrwilliams@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-03-16 21:41:40 +0000

    Added DSC5401 web interface code.  Written by Gaeton Lord et al.

---
+++ web/bin/DSC5401_status.pl
@@ -0,0 +1,57 @@
+# DSC5401_status.pl
+#
+# $Revision$
+# $Date$
+
+my $html=qq[
+<html>
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function setBullets() {
+
+content=parent.content_frame.document;
+
+if (content.images.length == 0) {
+  // content frame hasn't been loaded yet
+  return;
+}
+];
+
+my @name = DSC5401->ZoneName;
+my $size = scalar(@name)-1 ;
+
+$html.=qq[
+];
+for ( 1 .. $size ) {
+  if ( $name[$_] ) {
+    $html.=qq{content.images["ZONE_$_"].src="};
+    my $status = $DSC->{zone_status}{$_};
+    if ($status eq 'restored') {
+      $html .= '/graphics/green_bullet.gif';
+    } else { 
+      $html .= '/graphics/red_bullet.gif';
+    }
+    $html .= qq[";\n];
+  }
+}
+$html .= qq[
+}
+
+function doInit() {
+setBullets();
+setTimeout("doTimer()",2000);
+}
+
+function doTimer() {
+location.href="DSC5401_status.pl";
+}
+-->
+</script>
+<body onload="doInit()">
+</body>
+</html>
+];
+
+return &html_page( '', $html );
+
+    
---
+++ web/bin/DSC5401_web.pl
@@ -0,0 +1,16 @@
+# DSC5401_web.pl
+#
+# $Revision$
+# $Date$
+
+my $html='';
+
+$html .= qq[
+<html>
+<frameset rows="0%,100%"> 
+  <frame name="status_frame" frameborder=0 noresize scrolling=none src="DSC5401_status.pl">
+  <frame name="content_frame" frameborder=0 noresize scrolling=none src="DSC5401_web_content.pl">
+</frameset>
+</html>
+];
+return &html_page( '', $html );
