0c14ac3d6f0b8e2267ea566079177c0b126d4beb
mattrwilliams <mattrwilliams@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-03-23 20:52:32 +0000

    Added xPL support to slimserver connectivity options (slimserver now has native xPL capability). Added "slimdisplay" text command to allow easy testing of slimserver display capabilities.

--- code/common/display_slimserver.pl
+++ code/common/display_slimserver.pl
@@ -1,4 +1,6 @@
 # Category = MisterHouse
+# $Revision$
+# $Date$
 
 #@ Echos spoken text to a Slimp3 display ( http://www.slimdevices.com ).
 #@ Example mh.ini parms:  slimserver_clients=192.168.0.60:69,192.168.0.61:69  slimserver_server=192.168.0.2:9000
@@ -9,6 +11,10 @@
 &Speak_pre_add_hook(\&slimserver_display, 0) if $Reload;
 
 #speak "The time is $Time_Now" if new_second 15;
+
+my $slimdisplay=new Text_Cmd('slimdisplay\s*(.*)');
+$slimdisplay->set_casesensitive() if $Reload;
+$slimdisplay->tie_event('slimserver_display((text=>$state))') if $Reload;
 
 sub slimserver_display {
     my (%parms) = @_;
@@ -27,6 +33,16 @@ sub slimserver_display {
         my $duration = $parms{duration} || 15;
         &xAP::send('xAP', 'xAP-OSD.Display', 'Display.SliMP3' =>
                    {Line1 => $parms{text}, Line2 => ' ', Duration => $duration, Size => 'Double', Brightness => 'Brightest'});
+    }
+    elsif (lc($config_parms{slimserver_protocol}) eq 'xpl') { 
+      my $duration=$parms{duration} || 30;
+      $config_parms{slimserver_players} = $config_parms{slimserver_player} unless $config_parms{slimserver_players};
+      my $text=$parms{text};
+      $text="\\n$text" if $text !~ /\n/;
+      for my $player (split ',', $config_parms{slimserver_players}) {
+	$player =~ s/^\s*(.*)\s*$/$1/;
+        &xAP::send('xPL', "slimdev-slimserv.$player", 'osd.basic' => { command => 'write', text => $text, delay=> $duration});
+        }
     }
     else {
 				# Allow for player and/or players parm.  No Big or Brightest option here :(
