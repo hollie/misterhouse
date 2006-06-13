9fcb9b27be434117e5af17189b860069110cd7ff
dmark468 <dmark468@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-06-13 01:35:44 +0000

    Rewrite

--- lib/Weather_Item.pm
+++ lib/Weather_Item.pm
@@ -2,8 +2,8 @@ use strict;
 
 package Weather_Item;
 
-# $x = new Weather_Item(TempIndoor);     # returns e.g. 68/82/etc
-# $x = new Weather_Item(TempIndoor, '>' ,99) # returns e.g. false/true
+# $w_x = new Weather_Item(TempIndoor);     # returns value (eg 72)
+# $w_x = new Weather_Item('TempIndoor > 99') # returns evaluated expression if defined else return value undefined
 
 @Weather_Item::ISA = ('Generic_Item');
 my @weather_item_list;
@@ -12,11 +12,12 @@ sub Init {
     &::MainLoop_pre_add_hook(  \&Weather_Item::check_weather, 1 );
 }
 
+# *** Trigger
+
 sub check_weather {
     if($::New_Msecond_250)
     {
         for my $self (@weather_item_list) {
-            next unless defined $main::Weather{$self->{type}};
             my $state = $self->state; # Gets current state
             if (!defined $self->{state} or $self->{state} ne $state) {
                 &Generic_Item::set_states_for_next_pass($self,  $state);
@@ -25,40 +26,74 @@ sub check_weather {
     }
 }
 
+
+sub item_transform($) {
+    $_ = shift;
+    ($_ =~ /^(and|or|not|eq|ne|clear|cloudy|sunny|partly|mostly)$/i)?"$_":"\$::Weather{$_}";
+}
+
 sub new {
-    my ($class, $type) = @_;
+   my ($class, $type) = @_;
+   my @members;
 
-                                # Allow for 'Wind > 10' type tests
-    my ($comparison, $limit);
-    if ($type =~ /(\S+) *([\=\<\>]) *(\S+)/) {
-        $type       = $1;
-        $comparison = $2;
-        $limit      = $3;
-    }
-                                # Simple check for blanks (in case we used a bad operated above)
-                                # Too early to check for a valid %Weather key :(
-    if($type =~ / /) {
-        print "Invalid Weather_Item type: $type.\n";
-        return;
-    }
+   if ($type) {
+      $type =~ s/\x20+/\x20/g; 				# consolidate spaces
+      $type =~ s/([^0-9 \W]+)/item_transform($1)/egi; 	# markup items
+      $type =~ s/(partly cloudy|partly sunny|mostly cloudy|mostly sunny|clear|cloudy)/"'" . ucfirst(lc($1)) . "'"/egi;	 				# normalize condition strings
+      $type =~ s/ = '/ eq '/gi; 			# quote condition strings
 
-    my $self = {type => $type, comparison => $comparison, limit => $limit};
+      $type =~ s/[^<>]=/==/g;				# double equal signs (no assignments allowed)
+	# *** test with super syntax as well as module function calls
+
+      $type =~ s/&[^0-9 \W]+:{0,}[^0-9 \W]+\(.*\)//g;	# no function calls either (for safety)
+      $type =~ s/(state|item_transform|check_weather)//gi; # short-circuit methods (hack)
+
+      eval $type;					# validate
+      if ($@) {
+         warn "Weather_Item:$type did not evaluate (" . $@ . ')';	# error in expression
+      }
+      else { 						# validated, create item
+	# Save weather hash keys in member list (to be checked for existence in state sub before expression is evaluated)
+
+	    while ($type =~ /\$::Weather{(.*?)}/g) {		
+		push @members, $1 if !grep $1 eq $_, @members;	
+	    }
+      }
+   }
+   else {
+      warn 'Empty expression is not allowed.';
+   }
+
+
+    my $self = {type => $type, list => @members};
     bless $self, $class;
     push @weather_item_list, $self;
-    return $self;
+    return $self;         
+
+
 }
 
 sub state {
     my ($self) = @_;
-    return undef unless defined $main::Weather{$self->{type}};
-    return $main::Weather{$self->{type}} unless $self->{comparison};
-    return ($main::Weather{$self->{type}} <  ($self->{limit}) ? 1 : 0) if $self->{comparison} eq '<';
-    return ($main::Weather{$self->{type}} >  ($self->{limit}) ? 1 : 0) if $self->{comparison} eq '>';
-    return ($main::Weather{$self->{type}} == ($self->{limit}) ? 1 : 0) if $self->{comparison} eq '=';
+    my $valid;
+
+    $valid = 1;
+
+    # check that all members are defined
+
+    my @members = $self->{list};
+
+    for (@members) { # short-circuit evaluation if any member is undefined
+	$valid = 0 if !defined $main::Weather{$_};
+    }
+
+    # evaluate expression if so
+
+    return eval $self->{type} if $valid;
 }
 
 sub default_setstate {
-    print "Sorry, unable to control the weather.\n";
+    warn "Unable to control the weather.";
     return -1;
 }
 
