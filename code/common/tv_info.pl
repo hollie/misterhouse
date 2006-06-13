f11fc3e44a0ae812bdf1b60c23c6fbacaa0b76d9
dmark468 <dmark468@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-06-13 00:57:01 +0000

    Numerous bug fixes and updated to work with fixed get_tv_infp
    Still needs some more work (like ignoring skipped channels!)

--- code/common/tv_info.pl
+++ code/common/tv_info.pl
@@ -5,53 +5,66 @@
 
 $f_tv_file = new File_Item("$config_parms{data_dir}/tv_info1.txt");
 
-$v_tv_results = new  Voice_Cmd '[What are,Show] the tv search results';
-$v_tv_movies1 = new  Voice_Cmd('What TV movies are on channel [$config_parms{favorite_tv_channels}] tonight');
-$v_tv_movies2 = new  Voice_Cmd('What TV movies are on at [6pm,7pm,8pm,9pm] tonight');
-$v_tv_movies1-> set_info('Looks for TV shows that are 2-3 hours in length from 6 to 10 pm, on channels:');
-$v_tv_movies2-> set_info('Looks for TV shows that are 2-3 hours in length on all channels, at time:');
+$v_tv_results = new Voice_Cmd('What are the tv search results', 0);
+$v_tv_movies1 = new Voice_Cmd("What TV movies are on channel [$config_parms{favorite_tv_channels}] tonight", 0);
+$v_tv_movies2 = new Voice_Cmd('What TV movies are on at [6pm,7pm,8pm,9pm] tonight', 0);
+$v_tv_movies1-> set_info('Looks for TV shows that are 2-3 hours in length from 6 to 10 pm, on channels:', 0);
+$v_tv_movies2-> set_info('Looks for TV shows that are 2-3 hours in length on all channels, at time:', 0);
 
-$v_tv_shows1 = new  Voice_Cmd('What TV shows are on channel [$config_parms{favorite_tv_channels}] tonight');
-$v_tv_shows2 = new  Voice_Cmd('[Show,What are the] favorite TV shows on today');
-$v_tv_shows1-> set_info('Lists all shows on from 6 to 10 pm tonight on channel:');
+$v_tv_shows1 = new Voice_Cmd("What TV shows are on channel [$config_parms{favorite_tv_channels}] tonight");
+$v_tv_shows2 = new Voice_Cmd('What TV shows of interest are on today');
+$v_tv_shows1-> set_info("Lists all shows on from 6 to 10 pm tonight on channels:$config_parms{favorite_tv_channels}");
 $v_tv_shows2-> set_info("Checks to see if any of the following shows in $config_parms{favorite_tv_shows} are on today");
 
-if ($state = said  $v_tv_movies1) {
-    run qq[get_tv_info -length 2-3 -times 6pm+4 -channels $state];
+if (my $state = said $v_tv_movies1) {
+    $v_tv_movies1->respond("app=tv Searching for TV movies...");
+    run qq[get_tv_info -lengths 1.5-3 -times 6pm+4 -channels $state];
     set_watch $f_tv_file;
 }
-if ($state = said  $v_tv_movies2) {
-    run qq[get_tv_info -length 2-3 -times $state+.5];
+if (my $state = said $v_tv_movies2) {
+    $v_tv_movies2->respond("app=tv Searching for TV movies...");
+    run qq[get_tv_info -lengths 1.5-3 -times $state+.5];
     set_watch $f_tv_file;
 }
 
-if ($state = said  $v_tv_shows1) {
+if (my $state = said $v_tv_shows1) {
+    $v_tv_shows1->respond("app=tv Searching for shows of interest...");
     run qq[get_tv_info -channels $state];
     set_watch $f_tv_file;
 }
-if ($state = said $v_tv_shows2) {
-    print_log "Searching for favorite shows";
-    run qq[get_tv_info -times all -keys "$config_parms{favorite_tv_shows}" -keyfile $config_parms{favorite_tv_shows_file} -title_only];
-    set_watch $f_tv_file "$state favorites today";
-}
-elsif (time_cron('0 0 * * *') or $Reload) { #Refresh favorite shows today at midnight
-    print_log "Searching for favorite shows on today";
-    run qq[get_tv_info -times all -keys "$config_parms{favorite_tv_shows}" -keyfile $config_parms{favorite_tv_shows_file} -title_only -outfile2 $config_parms{data_dir}/web/tv_info3.txt];
+if (my $state = said $v_tv_shows2) {
+    respond "app=tv Searching for shows of interest...";
 
+    my ($min, $hour, $mday, $mon) = (localtime(time))[1,2,3,4];
+
+
+    run qq[get_tv_info -keys "$config_parms{favorite_tv_shows}" -keyfile $config_parms{favorite_tv_shows_file} -title_only -early_am 0+6 -times all];
+
+
+    set_watch $f_tv_file "$state favorites today";
+}  					# *** trigger! *** use process items, not file items
+elsif (time_cron('1 * * * *') or $Reload) { #Refresh favorite shows today at one after each hour (so as to prune ended shows from the Web page)
+    print_log "Searching for favorite shows on today";
+    run qq[get_tv_info -times all -keys "$config_parms{favorite_tv_shows}" -keyfile $config_parms{favorite_tv_shows_file} -title_only -early_am 0+6 -outfile2 $config_parms{data_dir}/web/tv_info3.txt];
+
+	# set "watch" (use process instead) and set $Info{tv_favorites} or whatever
 }
+
 
                                 # Check for favorite shows ever 1/2 hour
 if (time_cron('58,28 * * * *')) {
     my ($min, $hour, $mday, $mon) = (localtime(time + 120))[1,2,3,4];
     $mon++;
-    run qq[get_tv_info -quiet -times $hour:$min -dates $mon/$mday -keys "$config_parms{favorite_tv_shows}"  -keyfile $config_parms{favorite_tv_shows_file}  -title_only];
+    run qq[get_tv_info -quiet -times $hour:$min -dates $mon/$mday -keys "$config_parms{favorite_tv_shows}"  -keyfile $config_parms{favorite_tv_shows_file} -title_only];
     set_watch $f_tv_file 'favorites now';
 }
 
 
                                 # Search for requested keywords
-&tk_entry('TV search', \$Save{tv_search}, 'TV dates', \$Save{tv_days});
-if ($Tk_results{'TV search'} or $Tk_results{'TV dates'}) {
+&tk_entry('TV Search', \$Save{tv_search});
+&tk_entry('TV Dates', \$Save{tv_days});
+
+if ($Tk_results{'TV Search'} or $Tk_results{'TV Dates'}) {
     print_log "Searching TV programs...";
     run qq[get_tv_info -times all -dates "$Save{tv_days}" -keys "$Save{tv_search}"];
     set_watch $f_tv_file;
@@ -61,15 +74,21 @@ if ($Tk_results{'TV search'} or $Tk_results{'TV dates'}) {
 
                                 # Speak/show the results for all of the above requests
 
-
+# *** need to untangle this mess
 
 
 if (($state = changed $f_tv_file) or (my $state2 = said $v_tv_results)) {
     my $f_tv_info2 = "$config_parms{data_dir}/tv_info2.txt";
 
-    my $summary = read_head $f_tv_file 6;
+	# *** This is BS.  The read_head logic doesn't always work!
+
+#    my $summary = read_head $f_tv_file 6;
+    my $summary = read_all $f_tv_file;
     my ($show_count) = $summary =~ /Found (\d+)/;
-    $summary = "Found $show_count TV shows:";
+
+    if (defined $show_count) {
+    
+    $summary = "Found $show_count TV show" . (($show_count == 1)?'':'s') . (($show_count)?':':'.');
     my @data = read_all $f_tv_file;
     shift @data;            # Drop summary;
 
@@ -77,46 +96,50 @@ if (($state = changed $f_tv_file) or (my $state2 = said $v_tv_results)) {
     my $list = '';
     foreach my $line (@data) {
 
-        if (my ($title, $channel, $start, $end) =
-          $line =~ /^\d+\.\s+(.+)\.\s+\S+\s+Channel (\d+).+From ([0-9: APM]+) till ([0-9: APM]+)\./) {
+        if (my ($title, $channel, $channel_number, $start, $end) =
+          $line =~ /^\d+\.\s+(.+)\.\s+(\S+)\s+Channel (\d+).+From ([0-9: APM]+) till ([0-9: APM]+)\./) {
+		if ($channel) {
+			$channel = "$channel ($channel_number)";
+		}
+		else {
+			$channel = "channel $channel_number";
+		}
 
+		$channel = $channel . ' (in progress)' if $line =~ /(in progress.)/i;
+		
             if ($state eq 'favorites now') {
-                $list .= "$title Channel $channel.\n";
+                $list .= "$title $channel.\n";
             }
             else {
-                $list .= "$start Channel $channel $title.\n";
+                $list .= "$start $channel $title.\n";
             }
         }
         $i++;
     }
 
-    my $msg = "There ";
+    my $msg = "There";
     $msg .= ($show_count > 1) ? " are " : " is ";
-    $msg .= plural($show_count, 'favorite show');
+    $msg .= ($show_count . (($show_count == 1)?' show':' shows') . ' of interest');
     if ($state =~ 'favorites today') {
         if ($show_count > 0) {
-	    if ($state =~ 'What') {
-            	respond "target=speak app=tv $msg on today. $list";
-	    }
-	    else {
-		respond "app=tv $msg on today. $list\n";
-            }
+           	respond "app=tv $msg on today.\n$list";
+		$Save{tv_favorites} = "$msg on today.";
         }
         else {
-                respond "target=" . ((($state =~ 'What'))?'speak':'') . " app=tv There are no favorite shows on today";
+                respond "app=tv There are no shows of interest on today";
         }
     }
     elsif ($state eq 'favorites now') {
-        speak "app=tv Notice, $msg starting now.  $list" if $show_count > 0;
+        speak "app=tv force_chime=1 Notice, " . lcfirst($msg) . " starting now.\n$list" if $show_count > 0;
     }
     else {
-        chomp $summary;         # Drop the cr
-	if ($state2 eq 'Show') {
-        	respond "app=tv $summary $list ";
-	}
-	else {
-		respond "app=tv target=speak $summary $list ";
-	}
+        chomp $summary;         # Drop CR	
+	respond "app=tv no_chime=1 $summary $list ";
+	
+    }
+    }
+    else {
+	warn 'Error retrieving TV search results.';
     }
 
 }
