7fbde61acdd5b02acaaf6477d5ee9cf7ab52a435
dmark468 <dmark468@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-06-20 21:48:17 +0000

    Responses and empty report handling Object names were reverse reverse Hungarian (or something like that.)

--- code/common/internet_starshine.pl
+++ code/common/internet_starshine.pl
@@ -25,32 +25,52 @@ Note: Correct long. and time_zone parms for those of us in the
 
 =cut
 
-$starshine_check = new Voice_Cmd '[get,list,browse] starshine flares';
-$starshine_check ->set_info('Lists times and locations of visible starshine 3 satellite passes');
+$v_starshine_check = new Voice_Cmd '[Get,List,Browse] starshine flares';
+$v_starshine_check ->set_info('Lists times and locations of visible starshine 3 satellite passes');
 
-run_voice_cmd 'get starshine flares' if $New_Week;
+# Create trigger
+
+if ($Reload and $Run_Members{'trigger_code'}) {
+	my $command = '\$New_Week';
+	eval qq(
+            &trigger_set("$command", "run_voice_cmd('Get starshine flares')", 'NoExpire', 'get starshine info') 
+              unless &trigger_get('get starshine info');
+        );
+}
+
+sub uninstall_internet_starshine {
+	&trigger_delete('get starshine info');
+}
+
 
                                 # Their web site uses dorky Time Zone strings,
                                 # so use UCT (GMT+0) and translate.
 my $starshine_check_e = "$Code_Dirs[0]/starshine_check_events.pl";
-my $starshine_check_f = "$config_parms{data_dir}/web/starshine.html";
+my $f_starshine_check = "$config_parms{data_dir}/web/starshine.html";
 my $starshine_check_u = "http://www.heavens-above.com/PassSummary.asp?" . 
-                      "lat=$config_parms{latitude}&lng=$config_parms{longitude}&alt=0&TZ=UCT&satid=26929";
-$starshine_check_p = new Process_Item qq[get_url "$starshine_check_u" "$starshine_check_f"];
-#$starshine_check_p = new Process_Item "get_url '$starshine_check_u' '$starshine_check_f'";
+"lat=$config_parms{latitude}&lng=$config_parms{longitude}&alt=0&TZ=UCT&satid=26929";
+$p_starshine_check = new Process_Item qq[get_url "$starshine_check_u" "$f_starshine_check"];
 
-$state = said $starshine_check;
-start   $starshine_check_p if $state eq 'get';
-browser $starshine_check_f if $state eq 'browse';
+sub respond_starshine {
+	my $connected = shift;
+	my $display = &list_starshine();
+	if ($display) {
+		$v_starshine_check->respond("app=starshine connected=$connected Listing starshine data.");	
+		display $display, 0, 'Starshine list', 'fixed';
+	}
+	else {
+		$v_starshine_check->respond("app=starshine connected=$connected Nothing to report.");
+	}
+}
 
-if (done_now $starshine_check_p or $state eq 'list') {
+sub list_starshine {
+
     my ($display, $time, $sec, $time_sec);
-    my $html = file_read $starshine_check_f;
-                                # Add a base href, so we can click on links
+    my $html = file_read $f_starshine_check;
+                                # Add a base href, so we can click links
     $html =~ s|</head>|\n<BASE href='http://www.heavens-above.com/'>|i;
-    file_write $starshine_check_f, $html;
+    file_write $f_starshine_check, $html;
 
-#   my $text = HTML::FormatText->new(lm => 0, rm => 150)->format(HTML::TreeBuilder->new()->parse($html));
     my $text = &html_to_text($html);
 
     open(MYCODE, ">$starshine_check_e") or print_log "Error in writing to $starshine_check_e";
@@ -71,31 +91,50 @@ if (done_now $starshine_check_p or $state eq 'list') {
             if ($Dark and time_now '$time - 0:02') {
                 my \$msg = "Notice: Starshine 3 satellite will have a flare in 2 minutes ";
                 \$msg .= "at an altitude of $a[4], azimuth of $a[5].";
-                speak "app=timer \$msg";
+                speak "app=starshine \$msg";
                 display \$msg, 600;
-                set \$starshine_timer 120 + $sec;
+                set \$t_starshine_timer 120 + $sec;
             }
 eof
 
         }
     }
     close MYCODE;
-    display $display, 0, 'Starshine list', 'fixed';
-#   display $starshine_check_e;
-    do_user_file $starshine_check_e; # This will enable the above MYCODE 
+    return $display; 
+    
+    do_user_file $starshine_check_e; # This will enable the code file written above (MYCODE)
 }
 
+
+if (said $v_starshine_check) {
+	my $state = $v_starshine_check->{state};
+	my $state2 = $state;
+	$state2 = 'Brows' if $state2 eq 'Browse';
+	$state2 = 'Gett' if $state2 eq 'Get';
+	start $p_starshine_check if $state eq 'Get';
+	&browser($f_starshine_check) if $state eq 'Browse';
+	if ($state eq 'List') {
+		&respond_starshine(1);
+	}
+	else {
+		$v_starshine_check->respond("app=starshine $state2" . 'ing starshine report...');
+	}
+}
+
+
+&respond_starshine(0) if (done_now $p_starshine_check);
+
                                 # This timer will be triggered by the timer set in the above MYCODE
-$starshine_timer = new Timer;
+$t_starshine_timer = new Timer;
 my %starshine_timer_intervals = map {$_, 1} (15,30,90);
-if ($New_Second and my $time_left = int seconds_remaining $starshine_timer) {
+if ($New_Second and my $time_left = int seconds_remaining $t_starshine_timer) {
     if ($starshine_timer_intervals{$time_left}) {
         my $pitch = int 10*(1 - $time_left/60);
-        speak "app=timer pitch=$pitch $time_left seconds till flash";
+        speak "app=starshine pitch=$pitch $time_left seconds till flash...";
     }
 }
-if (expired $starshine_timer) {
-    speak "app=timer pitch=10 Starshine flash now occuring";
+if (expired $t_starshine_timer) {
+    speak "app=starshine pitch=10 Starshine flash now occuring!";
     play 'timer2';              # Set in event_sounds.pl
 }
 
