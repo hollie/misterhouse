faabdc1b00459c375518ead3c7e5a4ba939d587d
dmark468 <dmark468@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-06-21 04:43:59 +0000

    Defused a bomb on next/previous without start.  Added responses.  Consolidated three reload checks to a noloop block.

--- code/common/tk_photos.pl
+++ code/common/tk_photos.pl
@@ -17,69 +17,78 @@ See mh/bin/mh.ini for the various photo_* parms
 =cut
 
 use Tk;
-my_use "Tk::JPEG" if $Reload;           # Optional, in case it is not installed
-my_use "Tk::CursorControl" if $Reload;  # Optional, in case it is not installed
 
-$photo_slideshow         = new Voice_Cmd '[Start,Stop,Next,Previous] the photo slideshow';
+#noloop=start
+my_use "Tk::JPEG";            # Optional, in case it is not installed
+my_use "Tk::CursorControl";   # Ditto
 
-$photo_slideshow_timer   = new Timer;
-$photo_slideshow_process = new Process_Item;
+$v_photo_slideshow = new Voice_Cmd('[Start,Stop,Next,Previous] the photo slideshow');
 
-use vars '@photos';             # This will be persistent across passes and code reloads
-if ($Reload) {
-    @photos = file_read $config_parms{photo_index} unless @photos;
-    $config_parms{photo_time} = 60 unless defined $config_parms{photo_time};
-}
+$t_photo_slideshow = new Timer;
+$p_photo_slideshow = new Process_Item;
+
+use vars '@photos';             # This will be persistent across code reloads
+@photos = file_read $config_parms{photo_index} unless @photos;
+$config_parms{photo_time} = 60 unless defined $config_parms{photo_time};
 
 my ($mw_photo, $mw_photo_label, $mw_photo_image, $mw_cursor);
-
-if (said $photo_slideshow eq 'Start') {
-                                # Create a full screen, frameless window
-    unless ($MW) {
-        $MW = MainWindow->new;
-        $MW->geometry('0x0+0+0');
-    }
-    eval '$mw_cursor = $MW->CursorControl';       # Initialise the CursorControl
-    $mw_photo = $MW->Toplevel(-bg => "black");
-    $mw_photo->overrideredirect(1);
-    geometry $mw_photo $MW->screenwidth . "x" . $MW->screenheight . "+0+0";
-
-    $mw_photo -> bind("<q>", sub { run_voice_cmd 'Stop the photo slideshow' });
-    $mw_photo -> bind("<n>", sub { &photo_change('next')                    });
-    $mw_photo -> bind("<p>", sub { &photo_change('previous')                });
-    eval '$mw_cursor-> hide($mw_photo)';   # Hide the mouse cursor on this window
-
-    $mw_photo_label = $mw_photo -> Label(-border => 0) ->
-      pack(-anchor => 'center', -expand => 1);
-
-    &photo_change('next');
-}
-
-if (said $photo_slideshow eq 'Stop') {
-    eval '$mw_cursor-> show($mw_photo)';   # Show the cursor
-#   $MW             -> destroy  if $MW and ! $config_parms{tk};
-    $mw_photo       -> destroy  if $mw_photo;
-    $mw_photo_image -> delete   if $mw_photo_image;
-    undef $mw_cursor;
-    undef $mw_photo_image;
-    unset $photo_slideshow_timer;
-}
-
-if (said $photo_slideshow eq 'Next') {
-    &photo_change('next');
-}
-
-if (said $photo_slideshow eq 'Previous') {
-    &photo_change('previous');
-}
-
-
-&photo_change('next') if expired $photo_slideshow_timer;
-
 my $photo_temp_file = "$config_parms{data_dir}/mh_temp.tk_photo.jpg";
+
+#noloop=stop
+
+if (said $v_photo_slideshow) {
+	my $state = $v_photo_slideshow->{state};
+
+	if ($state eq 'Start') {
+    	    $v_photo_slideshow->respond('app=photos Starting the slideshow...');
+
+                                # Create a full screen, frameless window
+	    unless ($MW) {
+        	$MW = MainWindow->new;
+	        $MW->geometry('0x0+0+0');
+	    }
+	    eval '$mw_cursor = $MW->CursorControl';       # Initialise the CursorControl
+	    $mw_photo = $MW->Toplevel(-bg => "black");
+	    $mw_photo->overrideredirect(1);
+	    geometry $mw_photo $MW->screenwidth . "x" . $MW->screenheight . "+0+0";
+
+	    $mw_photo -> bind("<q>", sub { run_voice_cmd 'Stop the photo slideshow' }); # *** !
+	    $mw_photo -> bind("<n>", sub { &photo_change('next')                    });
+	    $mw_photo -> bind("<p>", sub { &photo_change('previous')                });
+	    eval '$mw_cursor-> hide($mw_photo)';   # Hide the mouse cursor on this window
+
+	    $mw_photo_label = $mw_photo -> Label(-border => 0) ->
+	    pack(-anchor => 'center', -expand => 1);
+	    &photo_change('next');
+
+	}
+	elsif ($state eq 'Stop') {
+		$v_photo_slideshow->respond('app=photos Stopping the slideshow...');	
+		eval '$mw_cursor-> show($mw_photo)';   # Show the cursor
+		#   $MW             -> destroy  if $MW and ! $config_parms{tk};
+		$mw_photo       -> destroy  if $mw_photo;
+		$mw_photo_image -> delete   if $mw_photo_image;
+		undef $mw_cursor;
+		undef $mw_photo_image;
+		undef $mw_photo;
+		unset $t_photo_slideshow;
+	}
+	elsif ($state eq 'Next') {
+   	    $v_photo_slideshow->respond('app=photos Next slide.');
+	    &photo_change('next');
+	}
+	elsif ($state eq 'Previous') {
+   	    $v_photo_slideshow->respond('app=photos Previous slide.');
+	    &photo_change('previous');
+	}
+}
+
+&photo_change('next') if expired $t_photo_slideshow;
+
+
 sub photo_change {
     my ($mode) = @_;
-    return unless $mw_photo;    # If mh restared, timer will still trigger
+    return unless $mw_photo;    # If mh restarted, timer will still trigger
     
     my $i    = $Save{photo_index};
     ($mode eq 'previous') ? $i-- : $i++;
@@ -94,8 +103,8 @@ sub photo_change {
                                 # Do it with a process, as it can take seconds
     if ($config_parms{photo_resize}) {
         my $size = $MW->screenwidth . "x" . $MW->screenheight;
-        set   $photo_slideshow_process qq|image_resize --size "$size" --file_in "$jpeg" --file_out "$photo_temp_file"|;
-        start $photo_slideshow_process;
+        set $p_photo_slideshow qq|image_resize --size "$size" --file_in "$jpeg" --file_out "$photo_temp_file"|;
+        start $p_photo_slideshow;
     }
                                 # This on-the-fly resize option causes mh to pause
     elsif (0) {
@@ -114,7 +123,7 @@ sub photo_change {
 
 }
 
-&photo_insert($photo_temp_file) if done_now $photo_slideshow_process;
+&photo_insert($photo_temp_file) if done_now $p_photo_slideshow;
 
 sub photo_insert {
     my ($jpeg) = @_;
@@ -136,5 +145,5 @@ sub photo_insert {
     else {
         $Save{photo_index_time} = $Time;
     }
-    set $photo_slideshow_timer $time;
+    set $t_photo_slideshow $time;
 }
