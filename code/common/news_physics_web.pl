65d52b5f122211632a15a33434fafa0024b638f7
dmark468 <dmark468@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-06-21 03:37:59 +0000

    Fixed up, but parse code is bogus.  Should be discarded or rewritten...

--- code/common/news_physics_web.pl
+++ code/common/news_physics_web.pl
@@ -7,36 +7,28 @@
 my $f_physics_web = "$config_parms{data_dir}/web/physics_web.txt";
 my $f_physics_web_html = "$config_parms{data_dir}/web/physics_web.html";
 
-#$f_physics_web2_html2 = new File_Item($f_physics_web2_html); # Needed if we use run instead of process_item
-
 $p_physics_web = new Process_Item("get_url http://physicsweb.org/archive/news $f_physics_web_html");
-$v_physics_web = new  Voice_Cmd('[Get,Read,Show] physics_web');
+$v_physics_web = new  Voice_Cmd('[Get,Read] physics web');
 $v_physics_web ->set_authority('anyone');
 
-respond($f_physics_web)   if said $v_physics_web eq 'Read';
-display($f_physics_web) if said $v_physics_web eq 'Show';
+respond($f_physics_web) if said $v_physics_web eq 'Read';
 
-if (said $v_physics_web eq 'Get') {
-
-    # Do this only if we the file has not already been updated today and it is not empty
-    if (0 and -s $f_physics_web_html > 10 and
-        time_date_stamp(6, $f_physics_web_html) eq time_date_stamp(6)) {
-        print_log "physics_web news is current";
-        display $f_physics_web;
-    }
-    else {
+if (said $v_physics_web eq 'Get') {    
         if (&net_connect_check) {
-            print_log "Retrieving Physics Web News Stories from the net ...";
-
+            $v_physics_web->respond("Retrieving Physics Web News Stories from the net ...");
             # Use start instead of run so we can detect when it is done
             start $p_physics_web;
         }        
-    }            
+	else {
+            $v_physics_web->respond("Connect to the Internet first!");
+	}
 }
 
 if (done_now $p_physics_web) {
     my $html = file_read $f_physics_web_html;                           
-    my ( $text, $count);
+    my ($text, $count);
+
+# *** This is broken!
 
     $text = "Physics Web News items: \n";
     for (file_read "$f_physics_web_html") {
@@ -58,6 +50,6 @@ if (done_now $p_physics_web) {
     $text =~ s![/[|\]]!!g;
     $text =~ s!\(.+\)!!g;
     file_write($f_physics_web, $text);
-    display $f_physics_web;
+    $v_physics_web->respond("connected=0 Physics web news retrieved.");
 }
 
