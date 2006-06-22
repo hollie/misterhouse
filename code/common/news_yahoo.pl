00715ff043390fcbcf008233039180fdfb7f1be5
dmark468 <dmark468@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-06-22 00:19:33 +0000

    Fixed responses and determined this is broken beyond repair (all it does now is download a page for IA5.)  See notes.

--- code/common/news_yahoo.pl
+++ code/common/news_yahoo.pl
@@ -1,12 +1,12 @@
- 
 #Category=News
 
 #@ This module adds functionality to obtain various categories of
 #@ news from yahoo.com, then read or display it.
 
-#my $f_all_news_html = "$config_parms{data_dir}/web/all_news.html";
-my $f_all_news_html = "$Pgm_Root/web/ia5/news/news.html";
+#my $f_all_news_html = "$config_parms{data_dir}/web/all_news.html"; # *** Makes more sense
+my $f_all_news_html = "$Pgm_Root/web/ia5/news/news.html"; # *** Why put this in IA5?
 my $f_Top_news = "$config_parms{data_dir}/web/Top_news.txt";
+
 my %news_files;
 $news_files{"US"} = "$config_parms{data_dir}/web/US_news.txt";
 $news_files{"World"} = "$config_parms{data_dir}/web/World_news.txt";
@@ -18,45 +18,48 @@ $news_files{"Health"} = "$config_parms{data_dir}/web/Health_news.txt";
 $news_files{"Sports"} = "$config_parms{data_dir}/web/Sports_news.txt";
  
 $p_all_news = new Process_Item("get_url http://dailynews.yahoo.com/fc $f_all_news_html");
+$v_all_news = new Voice_Cmd('Get current news');
+$v_all_news -> set_authority('anyone');
+$v_news = new Voice_Cmd('Tell me the [Top,U S,World,Entertainment,Business,Technology,Science,Health,Sports] news');
+$v_news -> set_authority('anyone');
  
-$v_all_news = new  Voice_Cmd('Get the current news');
-$v_all_news ->set_authority('anyone');
-$v_news     = new  Voice_Cmd('Tell me the [Top,U S,World,Entertainment,Business,Technology,Science,Health,Sports] news');
-$v_news     ->set_authority('anyone');
- 
-if ($state = said $v_news) {
-    respond($f_Top_news)                   if $state eq 'Top';
-    respond($news_files{"US"})             if $state eq 'U S';              
-    respond($news_files{"World"})          if $state eq 'World';            
-    respond($news_files{"Entertainment"})  if $state eq 'Entertainment';                     
-    respond($news_files{"Business"})       if $state eq 'Business';                   
-    respond($news_files{"Tech"})           if $state eq 'Technology';           
-    respond($news_files{"Science"})        if $state eq 'Science';                   
-    respond($news_files{"Health"})         if $state eq 'Health';                    
-    respond($news_files{"Sports"})         if $state eq 'Sports';           
+# Create trigger
+
+if ($Reload and $Run_Members{'trigger_code'}) {
+	eval qq(
+            &trigger_set("time_cron('13 6,12,18 * * *')", "run_voice_cmd('Get daily news')", 'NoExpire', 'get daily news') 
+              unless &trigger_get('get daily news');
+        );
+}
+
+# Events
+
+if (said $v_news) {
+    my $state = $v_news->{state};
+    $v_news->respond($f_Top_news)                  if $state eq 'Top';
+    $v_news->respond($news_files{"US"})            if $state eq 'U S';              
+    $v_news->respond($news_files{"World"})         if $state eq 'World';            
+    $v_news->respond($news_files{"Entertainment"}) if $state eq 'Entertainment'; 
+    $v_news->respond($news_files{"Business"})      if $state eq 'Business';                   
+    $v_news->respond($news_files{"Tech"})          if $state eq 'Technology';           
+    $v_news->respond($news_files{"Science"})       if $state eq 'Science';                   
+    $v_news->respond($news_files{"Health"})        if $state eq 'Health';                    
+    $v_news->respond($news_files{"Sports"})        if $state eq 'Sports';           
 }
  
-if ( said $v_all_news ) {
- 
-    # Do this only if we the file has not already been updated today and it is not empty
-    if (0 and -s $f_all_news_html > 10 and
-        time_date_stamp(6, $f_all_news_html) eq time_date_stamp(6)) {
-        print_log "The news is current";
-   respond("The news is current");
-    }
-    else {
+if (said $v_all_news) {   
         if (&net_connect_check) {
-            print_log "Retrieving the current news from the net ...";
-            respond("Retrieving the current news, please hold on.");
+            $v_all_news->respond('app=news Retrieving the current news...');
  
             # Use start instead of run so we can detect when it is done
             start $p_all_news;
         }        
-    }            
+ 	else {
+	    $v_all_news->respond('Cannot retrieve current news until connected to the Internet.');
+	}
 }
  
 if (done_now $p_all_news) {
-    respond("I have finished retrieving the news.");
     my $text;
     $text = "Here is what is making news right now: \n";
     for (file_read "$f_all_news_html") {
@@ -66,16 +69,24 @@ if (done_now $p_all_news) {
          }  
       }   
     file_write($f_Top_news, $text);
-    respond($f_Top_news);
+
+    if ($v_all_news->{state} ne 'Check') {
+	$v_all_news->respond("app=news connected=0 Current news retrieved.");
+    }
+    else {
+   	respond("app=news $text");
+    }
  
 open (FN,  "<$f_all_news_html");
  
+# *** Out of date and not worth saving.  This module is just a page downloader.
+
 my %news;
 while (<FN>) {
    if(m!<a href="http://story\.news\.yahoo\.com/fc\?cid=34&tmpl=fc&in=(\w+)&cat=[=&\w]+">!){
    my $cat = $1;
    $text = <FN>;
-   $text =~ s! <td width="30%" align=right valign=middle>\n!!g;
+   $text =~ s! <td width="30%" align="right" valign="middle">\n!!g;
    $text =~ s!</a></td>!.!g;
 #   chomp($text);
    if($text){push @{$news{"$cat"}}, "$text";}
