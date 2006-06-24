70d5a6b55a2cae27ef9804989372e128f2f8569f
dmark468 <dmark468@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-06-24 05:48:48 +0000

    Marketplace is still experimental.

---
+++ code/common/email_marketplace_sales.pl
@@ -0,0 +1,40 @@
+
+# Category = eCommerce
+
+#@ Check incoming email for sales.  Venues supported are: Amazon, Amazon Canada, Alibris and eBay
+#@ NOTE: Does nothing with multiple-item sales (names are not on the subject line.)
+#@ Results are announced and logged with the $CashRegister object.
+#@ Requires internet_mail.pl
+
+$cash_register = new Generic_Item;
+
+                                # get_email_scan_file and $p_get_email are created by internet_mail.pl
+if (done_now $p_get_email and -e $get_email_scan_file) {
+    print "marketplace: checking $get_email_scan_file\n" if $Debug{email};
+    my @msgs;
+    for my $line (file_read $get_email_scan_file) {
+	print "marketplace: mail =$line\n" if $Debug{email};
+        my ($msg, $from, $to, $subject, $body) = $line =~ /Msg: (\d+) From:(.+?) To:(.+?) Subject:(.+?) Body:(.+)/;
+        if ($subject =~ /^Sold, Ship Now (\d{5}) (.*)/i or $subject =~ /^Sold -- Ship Now (\d{5}) (.*)/i) {
+            speak "app=cashier $2 just sold on Amazon.";
+	    set $cash_register $2;
+	    print "marketplace: Found Amazon email: $subject\n" if $Debug{email};
+        }
+	elsif ($subject =~ /^eBay Store Inventory Sold: (.*) \(\d+\)/) {
+            speak "app=cashier $1 just sold in eBay Store.";
+	    set $cash_register $1;
+	    print "marketplace: Found eBay store email: $subject\n" if $Debug{email};		
+	}
+	elsif ($subject =~ /^eBay Item Sold: (.*) \(\d+\)/) {
+            speak "app=cashier $1 just sold on eBay.";
+	    set $cash_register $1;
+	    print "marketplace: Found eBay auction email: $subject\n" if $Debug{email};		
+	}
+	elsif ($subject =~ /^Alibris Purchase Notification # (\d+-\d+) - (.*)/) {
+            speak "app=cashier $2 just sold on Alibris.";
+	    set $cash_register $2;
+	    print "marketplace: Found Alibris email: $subject\n" if $Debug{email};		
+	}
+
+    }
+}
--- code/common/email_motion.pl
+++ code/common/email_motion.pl
@@ -7,7 +7,8 @@
 
 # Motion detected on Garage at 8:47 PM on 3/18/2004
 
-$email_motion_p = new Process_Item;
+$p_email_motion = new Process_Item;
+$v_email_motion = new Voice_Cmd 'Process email motion images';
 
                                 # get_email_scan_file and $p_get_email are created by internet_mail.pl
 if (done_now $p_get_email and -e $get_email_scan_file) {
@@ -24,16 +25,15 @@ if (done_now $p_get_email and -e $get_email_scan_file) {
                                 # Read in picture emails in the background
     if (@msgs) {
         print_log "Reading email_motion msgs @msgs";
-        set $email_motion_p qq[read_email -account bruce -msgnum "@msgs" -file "$config_parms{data_dir}/email_motion.txt"];
-        start $email_motion_p;
+        set $p_email_motion qq[read_email -account bruce -msgnum "@msgs" -file "$config_parms{data_dir}/email_motion.txt"];
+        start $p_email_motion;
     }
 }
 
-$email_motion_v = new Voice_Cmd 'Test email motion';
-
 use MIME::Base64; 
-if (done_now $email_motion_p or said $email_motion_v) {
-    print "Processing email_motion images\n";
+
+sub process_email_images {
+    print "Processing motion images receieved by email\n" if $Debug{email};
     my @data  = file_read "$config_parms{data_dir}/email_motion.txt";
 
     my $camera;
@@ -41,7 +41,7 @@ if (done_now $email_motion_p or said $email_motion_v) {
         my $r = shift @data;
         $camera = $1 if $r =~ /^ *Subject\: Motion\: (\S+)/;
         next unless $r =~ /^ *Content-Disposition/; # Assume this is the last record before the data
-        print " - reading data for $r\n";
+        print " - reading data for $r\n" if $Debug{email};
         shift @data; shift @data;  # Assume 2 blank blank header lines
         my $data;
         while (@data) {
@@ -53,9 +53,16 @@ if (done_now $email_motion_p or said $email_motion_v) {
         $Save{$index} = 1 if ++$Save{$index} > 100;
         my $member = $camera . sprintf "_%03d.jpg", $Save{$index};
         my $file = "$config_parms{data_dir}/web/motion/$member";
-        print " - writing to $file\n";
+        print " - writing to $file\n" if $Debug{email};
         file_write $file, MIME::Base64::decode($data);
         copy "$config_parms{data_dir}/web/motion/latest1.jpg", "$config_parms{data_dir}/web/motion/latest2.jpg";
         copy $file, "$config_parms{data_dir}/web/motion/latest1.jpg";
     }
 }
+
+if (said $v_email_motion) {
+	$v_email_motion->respond('Processing email motion captures');
+	&process_email_images();
+}
+
+&process_email_images() if done_now $p_email_motion;
