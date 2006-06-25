f253887ab04da6366178ca706c78c495275aff6b
dmark468 <dmark468@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-06-25 22:56:29 +0000

    Working now!

--- code/common/email_marketplace_sales.pl
+++ code/common/email_marketplace_sales.pl
@@ -7,30 +7,32 @@
 #@ Requires internet_mail.pl
 
 $cash_register = new Generic_Item;
+$v_email_marketplace = new Voice_Cmd 'Process email sales';
 
                                 # get_email_scan_file and $p_get_email are created by internet_mail.pl
-if (done_now $p_get_email and -e $get_email_scan_file) {
+if (done_now $p_get_email and -e $get_email_scan_file or said $v_email_marketplace) {
     print "marketplace: checking $get_email_scan_file\n" if $Debug{email};
     my @msgs;
     for my $line (file_read $get_email_scan_file) {
 	print "marketplace: mail =$line\n" if $Debug{email};
         my ($msg, $from, $to, $subject, $body) = $line =~ /Msg: (\d+) From:(.+?) To:(.+?) Subject:(.+?) Body:(.+)/;
-        if ($subject =~ /^Sold, Ship Now\. (\d{5}) (.*)/i or $subject =~ /^Sold -- Ship Now! (\d{5}) (.*)/i) {
+
+        if ($subject =~ /^ *Sold, Ship Now\. (\d{5}) (.*)/i or $subject =~ /^Sold -- Ship Now! (\d{5}) (.*)/i) {
             speak "app=cashier $2 just sold on Amazon.";
 	    set $cash_register $2;
 	    print "marketplace: Found Amazon email: $subject\n" if $Debug{email};
         }
-	elsif ($subject =~ /^eBay Store Inventory Sold: (.*) \(\d+\)/) {
+	elsif ($subject =~ /^ *eBay Store Inventory Sold: (.*) \(\d+\)/) {
             speak "app=cashier $1 just sold in eBay Store.";
 	    set $cash_register $1;
 	    print "marketplace: Found eBay store email: $subject\n" if $Debug{email};		
 	}
-	elsif ($subject =~ /^eBay Item Sold: (.*) \(\d+\)/) {
+	elsif ($subject =~ /^ *eBay Item Sold: (.*) \(\d+\)/) {
             speak "app=cashier $1 just sold on eBay.";
 	    set $cash_register $1;
 	    print "marketplace: Found eBay auction email: $subject\n" if $Debug{email};		
 	}
-	elsif ($subject =~ /^Alibris Purchase Notification # (\d+-\d+) - (.*)/) {
+	elsif ($subject =~ /^ *Alibris Purchase Notification # (\d+-\d+) - (.*)/) {
             speak "app=cashier $2 just sold on Alibris.";
 	    set $cash_register $2;
 	    print "marketplace: Found Alibris email: $subject\n" if $Debug{email};		
