4f61cf55ecff31f999eb3e5acb97dcd48a7b9a0f
dmark468 <dmark468@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-06-26 22:27:12 +0000

    Removed residue of old version.  Added new states.

--- lib/Mp3Player.pm
+++ lib/Mp3Player.pm
@@ -1,14 +1,8 @@
 #!/usr/bin/perl                                                                                 
 #
-#
-#
-# 
-#    bsobel@vipmail.com'
-#    August 15, 2000
-#
-#    Hacked by David Mark to bypass duplicate Winamp code and provide an object-based
-#    interface for all MP3 players.  Code for the different players should be moved into
-#    this object eventually (no need for four mutually exclusive common code modules.)
+#    Provides an object-based interface for all MP3 players.
+#    Code for the different players should be moved into this object eventually
+#    (no need for four mutually exclusive common code modules.)
 
 use strict;
 
@@ -20,13 +14,12 @@ my @mp3player_object_list;
 sub new 
 {
     my ($class, $address) = @_;
-
     my $self = {address => $address};
+
     bless $self, $class;
 
     push(@mp3player_object_list,$self);
-
-    push(@{$$self{states}}, 'play', 'pause', 'stop', 'next song', 'previous song', 'shuffle', 'repeat', 'random song', 'volume up', 'volume down', 'clear list');
+    push(@{$$self{states}}, 'play', 'pause', 'stop', 'next song', 'previous song', 'shuffle', 'repeat', 'volume up', 'volume down', 'queue', 'clear list', 'random song');
 
     return $self;
 }
@@ -34,60 +27,53 @@ sub new
 sub default_setstate
 {
     my ($self, $state) = @_;
-    print "Mp3Player set called: " . $self->{address} . " to " . $state . "\n" if $::Debug{mp3};
-    mp3_player_control($state,$self->{address});
-    return;
+    my $address = $self->{address} || 'localhost';
+
+    print "Mp3Player set: " . $self->{address} . " to " . $state . "\n" if $::Debug{mp3};
+
+	# NOTE: -1 is returned by the object to suppress the state change on failure.
+	# X10 and Insteon controllers need to work like this (cm11 doesn't.)
+
+	# 0 = failure goes to -1 for mh fail code;
+	# 1 = success goes to 0, which mh sees as a success. (?)
+
+    return mp3_player_control($state,$self->{address}) - 1;
 }
 
 sub mp3_player_control 
 {
     my ($command, $host) = @_;
+    my $result;
 
-    eval "&::mp3_control('$command', '$host')";
-
-    warn "mp3_control:$@\n" if $@;
-
-    return;
-
-    $host = 'localhost' unless $host;
-
-                                # Start winamp, if it is not already running (windows localhost only)
-    &::sendkeys_find_window('winamp ', $::config_parms{mp3_program}) if $::OS_win and $host eq 'localhost';
-
-    my $temp;
-    my $url = "http://$host:$::config_parms{mp3_program_port}";
-    if($command eq 'randomsong')
-    {
-        my $mp3_num_tracks = ::get "$url/getlistlength?p=$::config_parms{mp3_program_password}";
-        my $song = int(rand($mp3_num_tracks));
-        my $mp3_song_name  = ::get "$url/getplaylisttitle?p=$::config_parms{mp3_program_password}&a=$song";
-        $mp3_song_name =~ s/[\n\r]//g;
-        ::print_log "Now Playing $mp3_song_name";
-        ::get "$url/stop?p=$::config_parms{mp3_program_password}";
-        ::get "$url/setplaylistpos?p=$::config_parms{mp3_program_password}&a=$song";
-        ::print_log filter_cr get "$url/play?p=$::config_parms{mp3_program_password}";
+    if ($command =~ /^play:(.+)$/i) {
+	eval "\$result = &::mp3_play($1)";
     }
-    elsif($command =~ /playlist:/i)
-    {
-        my ($file) = $command =~ /playlist:(.+)/i;
+    elsif ($command =~ /^playlist:(.+)$/i) {
+	eval "\$result = &::mp3_playlist($1)";
+    }
+    elsif ($command =~ /^station:(.+)$/i) {
+	eval "\$result = &::mp3_radio_play($1)";
+    }
+    elsif ($command =~ /^queue:(.+)$/i) {
+	eval "\$result = &::mp3_queue($1)";
+    }
+    elsif ($command eq 'clear list') {
+	eval "\$result = &::mp3_clear()";
+    }
+    else {
+	eval "\$result = &::mp3_control('$command', '$host')";
+    }
 
-        ::print_log ::filter_cr ::get "$url/DELETE?p=$::config_parms{mp3_program_password}";
-        ::print_log ::filter_cr ::get "$url/PLAYFILE?p=$::config_parms{mp3_program_password}&a=$file";
-        ::print_log ::filter_cr ::get "$url/PLAY?p=$::config_parms{mp3_program_password}";
+    if ($@) {
+	    warn "mp3_control: eval error: $@\n";
+	    return 0; # Failed to evaluate
     }
-    elsif($command =~ /volume/i)
-    {
-        $temp = '';
-        # 10 passes is about 20 percent 
-        for my $pass (1 .. 10) 
-        {
-            $temp .= ::filter_cr ::get "$url/$command?p=$::config_parms{mp3_program_password}";
-        }
+    elsif (!$result) {
+	    warn "mp3_control: command failed: $command";
+	    return 0; # Failed to execute
     }
-    else 
-    {
-#        ::print_log "$url/$command?p=$::config_parms{mp3_program_password}";
-        $temp = ::filter_cr ::get "$url/$command?p=$::config_parms{mp3_program_password}";
+    else {
+	    return 1; # Success!
     }
 }
 
