ad936d46b6c47c6138a9ff0d5e69c8fc5c85d3c6
dmark468 <dmark468@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-06-30 03:08:41 +0000

    Changed default font on Windows to arial (can't miss.)

--- lib/Display.pm
+++ lib/Display.pm
@@ -163,10 +163,18 @@ sub display {
 #       $$self{font} = 'Courier* 10 bold' unless $$self{font};
 #       $$self{font} = 'system'           unless $$self{font};
 #       $$self{font} = 'systemfixed'      if     $$self{font} eq 'fixed';
-        $$self{font} = 'Times 10 bold'   unless $$self{font};
-        $$self{font} = 'Courier 10 bold'  if    $$self{font} eq 'fixed';
 
-                                # Valid fonts can be listed with xlsfonts 
+	$$self{font} = $main::config_parms{tk_display_font} if defined($main::config_parms{tk_display_font}) and $main::config_parms{tk_display_font};
+
+	if ($main::OS_win) {
+	        $$self{font} = 'arial'   unless $$self{font};
+	}
+	else {
+	        $$self{font} = 'Times 10'   unless $$self{font};
+	}
+        $$self{font} = 'Courier 10'  if    $$self{font} eq 'fixed';
+
+                                # Valid fonts can be listed with xlsfonts (need to!)  See font dialog...
         my $t1;
         #my $inserted;
 
