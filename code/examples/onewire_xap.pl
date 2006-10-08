3abf4acbb89775bfb3f1d1ff6b26a95c9ce0b17e
gliming <gliming@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-10-08 12:50:15 +0000

    Modified to illustrate use of AnalogSensor_Item tokens

--- code/examples/onewire_xap.pl
+++ code/examples/onewire_xap.pl
@@ -22,13 +22,15 @@ my $thermo_temps = "68,69,70,71,72,73,74,75,76,77,78,79,80";
 $v_thermo_cool_sp = new Voice_Cmd("Set thermostat cool setpoint to [$thermo_temps]");
 if ($state = said $v_thermo_cool_sp) {
    $main::Save{'cool_setpoint'} = $state;
-   &set_thermo_sensor($temp_basement);
+   $temp_inside->token('cool_setpoint', $state);
+   $temp_inside->check_tied_state_conditions();
 }
 
 $v_thermo_heat_sp = new Voice_Cmd("Set thermostat heat setpoint to [$thermo_temps]");
 if ($state = said $v_thermo_heat_sp) {
    $main::Save{'heat_setpoint'} = $state;
-   &set_thermo_sensor($temp_basement);
+   $temp_inside->token('heat_setpoint', $state);
+   $temp_inside->check_tied_state_conditions();
 }
 
 # setup additional properties not definable via .mht
@@ -36,10 +38,22 @@ if ($state = said $v_thermo_heat_sp) {
 # noloop=start
 
 # map sensors to weather hash
-$temp_outside->map_to_weather('TempOutdoor');
-$humid_outside->map_to_weather('HumidOutdoor');
 $temp_inside->map_to_weather('TempIndoor');
 $humid_inside->map_to_weather('HumidIndoor');
+
+# now, create the conditions ("rules") used for state transition
+# note the use of $token_<tokentag> syntax
+my $hot_condition = '$measurement > $token_cool_setpoint';
+my $comfort_condition = '$token_heat_setpoint <= $measurement and '
+       . '$measurement <= $token_cool_setpoint';
+my $cool_condition = '$measurement < $token_heat_setpoint';
+# now, tie the conditions
+$temp_inside->tie_state_condition($hot_condition, 'hot');
+$temp_inside->tie_state_condition($comfort_condition, 'comfort');
+$temp_inside->tie_state_condition($cool_condition, 'cool');
+# now that the conditions are tied, just update the token values
+# when needed; don't worry about untieing and retieing conditions; just,
+# set them once as above w/i this noloop block
 
 # compensate for miscalibrated temp sensor - partly due to heating inside case
 $temp_inside->apply_offset(-3.5);
@@ -52,33 +66,19 @@ if ($Reload) {
    # of mode and/or season
    $main::Save{'cool_setpoint'} = '80' unless $main::Save{'cool_setpoint'};
    $main::Save{'heat_setpoint'} = '68' unless $main::Save{'heat_setpoint'};
-   # evaluate tied state conditions
-   &set_thermo_sensor($temp_inside);
+
+   # add setpoint tokens to simpify condition statements
+   # note that this is also needed as tokens aren't persisted across restarts
+   $temp_inside->token('cool_setpoint', $main::Save{'cool_setpoint'});
+   $temp_inside->token('heat_setpoint', $main::Save{'heat_setpoint'});
+
+   # and, finally, force an evaluation so that state is set
+   $temp_inside->check_tied_state_conditions();
 }
 
 # print out values; useful for confirmation/troubleshooting
 if (new_minute 1) {
-	print "outside temp = " . $temp_outside->measurement . "F\n";
-	print "outside humid = " . $humid_outside->measurement . "%\n";
 	print "inside temp = " . $temp_inside->measurement . "F\n";
 	print "inside humid = " . $humid_inside->measurement . "%\n";
-}
-
-sub set_thermo_sensor {
-   my ($sensor) = @_;
-   print "Setting thermostat sensor state conditions\n";
-
-   my $hot_condition = '$measurement > ' . $main::Save{'cool_setpoint'};
-   my $comfort_condition = $main::Save{'heat_setpoint'} . 
-                        ' <= $measurement and $measurement <= ' . 
-                        $main::Save{'cool_setpoint'};
-   my $cool_condition = '$measurement < ' . $main::Save{'heat_setpoint'};
-   $sensor->untie_state_condition(); # untie all
-   # add state conditions
-   $sensor->tie_state_condition($hot_condition, 'hot');
-   $sensor->tie_state_condition($comfort_condition, 'comfort');
-   $sensor->tie_state_condition($cool_condition, 'cool');
-   # cause a state evaluation
-   $sensor->check_tied_state_conditions();
 }
 
