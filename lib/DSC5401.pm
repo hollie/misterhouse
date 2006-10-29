03f5651f85b1c67c7d6036afb9e8f629d7599bd1
mattrwilliams <mattrwilliams@71d350d0-f70e-0410-9bbe-9702f904aa17>
2006-10-29 14:22:48 +0000

    Added set_clock method to allow easier setting of the DSC system clock.

--- lib/DSC5401.pm
+++ lib/DSC5401.pm
@@ -915,6 +915,26 @@ sub cmd_list {
       &::print_log("$k");
    }
 }
+
+# This method copied from Gaeton's example DSC_Clock.pl code.
+# I recommend that this be run every day at 3am to keep the clock
+# synchronized and also to correct for daylight saving time.
+sub set_clock {
+   my ($self)=@_;
+    
+   my ($sec,$m,$h,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+   $year = sprintf("%02d", $year % 100);
+   $mon+=1;
+   $m=($m<10)?"0".$m:$m;
+   $h=($h<10)?"0".$h:$h;
+   $mday=($mday<10)?"0".$mday:$mday;
+   $mon=($mon<10)?"0".$mon:$mon;
+   my $TimeStamp="$h$m$mon$mday$year";
+   &::print_log("Setting time on DSC panel to $TimeStamp");
+   &::logit( "$main::config_parms{data_dir}/logs/DSC5401.$main::Year_Month_Now.log", "Setting time on DSC panel to $TimeStamp" );
+   $self->cmd("SetDateTime",$TimeStamp);
+}
+
 1;
 
 #}}}
