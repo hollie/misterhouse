2eca3e3f2972d756f04e5e12b34c63e8e9ba33bd
gliming <gliming@71d350d0-f70e-0410-9bbe-9702f904aa17>
2007-04-25 10:39:35 +0000

    Revised sending xAP weather messages to be more compliant (to include appropriate units switching) to the current schema

--- code/common/xAP_send.pl
+++ code/common/xAP_send.pl
@@ -14,13 +14,49 @@ $xap_echo_all = 1 unless defined $xap_echo_all;
                                 # Send weather data
 if (new_minute 1 and ($xap_echo_all or $config_parms{xap_echo_weather})) {
 # Scheme from: From: http://www.xapautomation.org/modules.php?name=Sections&op=viewarticle&artid=2
-    &xAP::send('xAP', 'weather.report', 'weather.report' => {
-          UTC  => "$Hour:$Minute",                DATE => scalar &time_date_stamp(19),
-        WindM  => $Weather{WindAvgSpeed},     WindDirC => $Weather{AvgDir}, WindGustsM => $Weather{WindGustSpeed},
-        TempF  => $Weather{TempOutdoor},   TempIndoorF => $Weather{TempIndoor},
-        HumidF => $Weather{HumidOutdoor},  HumidIndoor => $Weather{HumidIndoor},
-         DewF  => $Weather{DewOutdoor},    AirPressure => $Weather{Barom}
-    } );
+    my $f_date = $Year . ((length($Month) == 1) ? '0' : '') . $Month . ((length($Mday) == 1) ? '0' : '') . $Mday;
+    my $f_tod = ($Dark) ? 'Night' : 'Day';
+    my $f_time = ((length($Hour) == 1) ? '0' : '') . $Hour . ':' . ((length($Minute) == 1) ? '0' : '') . $Minute
+          . ':' . ((length($Second) == 1) ? '0' : '') . $Second;
+    my ($sunriseh, $sunrisem, $sunrisec) = $Time_Sunrise =~ /(\S+):(\S+) (\S+)/;
+    $sunriseh += 12 if $sunrisec =~ /pm/i;
+    my ($sunseth, $sunsetm, $sunsetc) = $Time_Sunset =~ /(\S+):(\S+) (\S+)/;
+    $sunseth += 12 if $sunsetc =~ /pm/i;
+    my $utctime = gmtime($Time);
+    my ($f_utc) = $utctime =~ /^\S+ \S+ \S+ (\d+:\d+):\d+ \S+$/;
+
+    my $windspeedc = ($config_parms{weather_uom_wind} =~ /mph/i) ? 'M' : 'K';
+    my $tempc = ($config_parms{weather_uom_temp} =~ /^f$/i) ? 'F' : 'C';
+
+    my $f_description = $Weather{Clouds} . (($Weather{Clouds}) ? ' ' : '') . $Weather{Conditions};
+    my $conditions = ($Weather{Conditions}) ? $Weather{Conditions} : $Weather{Clouds};
+    my $f_icon = '';
+    if ($conditions eq 'broken clouds') {
+       $f_icon = 'clouds1';
+    } elsif ($conditions =~ /clear/i) {
+       $f_icon = 'sunny';
+    }
+
+    my $f_winddir = &Weather_Common::convert_wind_dir_to_abbr($Weather{WindAvgDir});
+
+    my ($weatherrpt,$timerpt);
+    $weatherrpt->{'UTC'} = $f_utc;
+    $weatherrpt->{'DATE'} = $f_date;
+    $weatherrpt->{"Wind$windspeedc"} = $Weather{WindAvgSpeed} if exists $Weather{WindAvgSpeed};
+    $weatherrpt->{"WindGusts$windspeedc"} = $Weather{WindGustSpeed} if exists $Weather{WindGustSpeed};
+    $weatherrpt->{'WindDirD'} = $Weather{WindAvgDir} if exists $Weather{WindAvgDir};
+    $weatherrpt->{'WindDirC'} = $f_winddir;
+    $weatherrpt->{"Temp$tempc"} = $Weather{TempOutdoor} if exists $Weather{TempOutdoor};
+    $weatherrpt->{"Dew$tempc"} = $Weather{DewOutdoor} if exists $Weather{DewOutdoor};
+    $weatherrpt->{'AirPressure'} = $Weather{Barom} if exists $Weather{Barom};
+    $weatherrpt->{'Icon'} = $f_icon;
+    $weatherrpt->{'Description'} = $f_description if $f_description;
+    $timerpt->{'TOD'} = $f_tod;
+    $timerpt->{'Time'} = $f_time;
+    $timerpt->{'SunRise'} = "$sunriseh:$sunrisem";
+    $timerpt->{'SunSet'} = "$sunseth:$sunsetm";
+
+    &xAP::send('xAP', 'Weather.Report', 'Weather.Report' => $weatherrpt, 'Time' => $timerpt);
 }
 
                                 # Echo incoming X10 data
@@ -69,7 +105,15 @@ sub xAP_send_speak {
     my $tts_block;
     $tts_block->{'say'} = $$parms_ref{text};
     $tts_block->{'volume'} = $$parms_ref{'volume'} if defined $$parms_ref{'volume'};
-    $tts_block->{'voice'} = $$parms_ref{'voice'} if defined $$parms_ref{'voice'};
+    my $voice = $$parms_ref{'voice'};
+    if ($voice) {
+       my (%voice_names);
+       &main::read_parm_hash(\%voice_names, $main::config_parms{voice_names});
+       if (exists($voice_names{$voice})) {
+          $voice = $voice_names{$voice};
+       }
+    }
+    $tts_block->{'voice'} = $voice if $voice;
     if ($schema eq 'tts.speak') {
        my $priority = lc $$parms_ref{'priority'};
        if ($priority) {
@@ -79,13 +123,14 @@ sub xAP_send_speak {
              $tts_block->{'priority'} = 'no';
           }
        }
+       $tts_block->{'event'} = $$parms_ref{'rooms'} if defined $$parms_ref{'rooms'};
     } elsif ($schema eq 'mhtts.speak') {
        $tts_block->{'device'} = $$parms_ref{'card'} if defined $$parms_ref{'card'};
        $tts_block->{'mode'} = $$parms_ref{'mode'} if defined $$parms_ref{'mode'};
        $tts_block->{'app'} = $$parms_ref{'app'} if defined $$parms_ref{'app'};
        $tts_block->{'requestor'} = $$parms_ref{'requestor'} if defined $$parms_ref{'requestor'};
        $tts_block->{'priority'} = $$parms_ref{'priority'} if defined $$parms_ref{'priority'};
-    }
+   }
     &xAP::send('xAP', $schema, 'tts.speak' => $tts_block);
 }
 
