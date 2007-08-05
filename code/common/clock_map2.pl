6878c2c9280b3ce038702e46a8a5ce322967c23e
hplato <hplato@71d350d0-f70e-0410-9bbe-9702f904aa17>
2007-08-05 01:03:53 +0000

    added a file check to ensure that image files are not overwritten by 'file not found' message

--- code/common/clock_map2.pl
+++ code/common/clock_map2.pl
@@ -9,8 +9,9 @@
 
 my $URL = "http://www.fourmilab.ch/cgi-bin/uncgi/Earth?img=learth.evif&imgsize=150&dynimg=y&opt=-p&lat=&lon=&alt=&tle=&date=0&utc=&jd=";
 my $MAPDIR = "$Pgm_Root/web/newclock/maps";
-my $f_get_map = "$Pgm_Root/data/earth_tmp.jpg";
-$p_get_map = new Process_Item ("get_url -quiet \"$URL\" $Pgm_Root/data/earth_tmp.jpg"); 
+my $f_get_map = "$config_parms{data_dir}/earth_jpg.data";
+my $min_size = 20000; #minimum size to test if a jpg has been downloaded
+$p_get_map = new Process_Item ("get_url -quiet \"$URL\" $config_parms{data_dir}/earth_jpg.data"); 
 
 # Update earth.jpg for each Hour, once a week
 
@@ -28,7 +29,11 @@ if ((time_cron '1 * * * *') or (said $v_get_earth_clock))  {
 
 if (done_now $p_get_map) {
 	print_log "Clock_map: Uploading earth map for hour $Hour";
-	copy "$Pgm_Root/data/earth_tmp.jpg", "$MAPDIR/$Hour.jpg";
+        #only copy the jpg if successful
+        my $filesize = -s $f_get_map;
+	if ($filesize > $min_size) {
+	   copy "$f_get_map", "$MAPDIR/$Hour.jpg";
+	} else {
+	   print_log "Clock_map: Bad file size, file not copied";
+	}
 }
-
-
