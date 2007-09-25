875e2232114d766025585c47d7501cf5054e0864
zonyl <zonyl@71d350d0-f70e-0410-9bbe-9702f904aa17>
2007-09-25 03:42:21 +0000

    Queue Reliability Issues.  Fix X10 receive

--- lib/Insteon_PLM.pm
+++ lib/Insteon_PLM.pm
@@ -182,6 +182,25 @@ my %x10_commands = (
 						hail_request => 0x8
 );
 
+my %mh_commands = (
+						'2' => 'on',
+						'3' => 'off',
+						'5' => 'bright',
+						'4' => 'dim',
+						'a' => 'preset_dim1',
+						'b' => 'preset_dim2',
+						'0' => 'all_off',
+						'1' => 'all_lights_on',
+						'6' => 'all_lights_off',
+						'f' => 'status',
+						'd' => 'status_on',
+						'e' => 'status_off',
+						'9' => 'hail_ack',
+						'7' => 'ext_code',
+						'c' => 'ext_data',
+						'8' => 'hail_request'
+);
+
 sub serial_startup {
    my ($instance) = @_;
 
@@ -203,34 +222,10 @@ sub poll_all {
 }
 
 
-# Takes a hexadecimal string and calculates the checksum in a hexadecimal string
-sub get_checksumHex
-{
-	my ($msg) = @_;
-	my $bmsg;
-	$bmsg = pack("H*",$msg);
-    my @bytes = unpack 'C*', $bmsg;
-    my $checksum = 0;
-    foreach (@bytes[0..$#bytes]) {
-        $checksum += $_;
-    }
-    $checksum = ~$checksum;
-    $checksum++;
-    $checksum &= 0xff;
-	$checksum = sprintf("%02X",$checksum);
-
-	return $checksum;
-}
-
-
 sub check_for_data {
-#	&::print_log("PLM CFD:");
-#	for my $temp (keys %Insteon_PLM_Data) {
-#	&::print_log("PLM ARR:$temp:" . $Insteon_PLM_Data{$temp});
-#	}
+
    for my $port_name (keys %Insteon_PLM_Data) {
       &::check_for_generic_serial_data($port_name) if $::Serial_Ports{$port_name}{object};
-#      my $data = $::Serial_Ports{$port_name}{data_record};
       my $data = $::Serial_Ports{$port_name}{data};
       next if !$data;
 
@@ -289,16 +284,14 @@ sub set
 	my ($self,$p_state,$p_setby,$p_response) = @_;
 
 	my ($package, $filename, $line) = caller;
-	&::print_log("PLM xmit:" , $p_setby->{object_name} . ":$p_setby:$p_state:$package:$filename:$line");
+	&::print_log("PLM xmit:" , $p_setby->{object_name} . ":$p_state:$p_setby");
 	
 	#identify the type of device that sent the request
 	if ($p_setby->isa("X10_Item") or $p_setby->isa("X10_Switchlinc"))
 	{
-		&::print_log("PLM: XSW");
 		$self->_xlate_mh_x10($p_state,$p_setby);
 	} elsif ($p_setby->isa("Insteon_Device")) {
-		&::print_log("PLM: IPLD:$p_state");
-		$self->send_plm_cmd(pack("H*",'0262' . $p_state));
+		$self->send_plm_cmd('0262' . $p_state);
 	}
 }
 
@@ -306,16 +299,19 @@ sub send_plm_cmd
 {
 	my ($self, $cmd) = @_;
 	#queue any new commands
-	unshift(@{$$self{command_stack}},$cmd) if defined $cmd;
+	if (defined $cmd and $cmd ne '')
+	{
+		&::print_log("PLM Add Command:" . $cmd . ":XmitInProgress:" . $$self{xmit_in_progress} . ":" );
+		unshift(@{$$self{command_stack}},$cmd);
 
-	&::print_log("PLM Add Command:" . unpack("H*",$cmd) . ":XmitInProgress:" . $$self{xmit_in_progress} . ":" );
+	}
 	#we dont transmit on top of another xmit
 	if ($$self{xmit_in_progress} != 1) {
 		$$self{xmit_in_progress} = 1;
 		#TODO: Should start a timer just in case PLM is not responding and we need to clear xmit_in_progress after a while.
 		#always send the oldest command first
 		$cmd = pop(@{$$self{command_stack}});
-		if (defined $cmd) 
+		if (defined $cmd and $cmd ne '') 
 		{
 			#put the command back into the stack.. Its not our job to tamper with this array
 			push(@{$$self{command_stack}},$cmd);
@@ -329,44 +325,42 @@ sub send_plm_cmd
 sub _send_cmd {
 	my ($self, $cmd) = @_;
 	my $instance = $$self{port_name};
-	    my $hex = unpack "H*", $cmd;
 
-	&::print_log("PLM: Executing command:$hex:") unless $main::config_parms{no_log} =~/Insteon_PLM/;
-	my $data = $cmd;
+	&::print_log("PLM: Executing command:$cmd:") unless $main::config_parms{no_log} =~/Insteon_PLM/;
+	my $data = pack("H*",$cmd);
 	$main::Serial_Ports{$instance}{object}->write($data);
 ### Dont overrun the controller.. Its easy, so lets wait a bit
 #	select(undef,undef,undef,0.15);
     #X10 is sloooooow
 	select(undef,undef,undef,0.5);
    	$$self{'last_change'} = $main::Time;
-#	$self->_poll();
 }
 
 
 sub _parse_data {
 	my ($self, $data) = @_;
-##   return if (($$self{'last_change'} + 5) > $main::Time);
    my ($name, $val);
-#   $data =~ s/^\s*//;
-#   $data =~ s/\s*$//;
+
 	my $processedNibs=0;
 
-	&::print_log( "PLM: Parsing serial data: $data\n") unless $main::config_parms{no_log} =~/Insteon_PLM/;
+	&::print_log( "PLM: Parsing serial data: $data") unless $main::config_parms{no_log} =~/Insteon_PLM/;
 	
 	foreach my $data_1 (split(/(0263\w{6})|(0252\w{4})|(0250\w{18})|(0251\w{46})|(0262\w{14})/,$data))
 	{
+		#ignore blanks.. the split does odd things
+		next if $data_1 eq '';
 		#we found a matching command in stream, add to processed bytes
 		$processedNibs+=length($data_1);
 
 		#get the command on the stack that was last sent (it should be echo'd back to us for an ack/err)
-		my $prev_cmd = pop(@{$$self{command_stack}});
-		if (defined $prev_cmd) 
+		my $prev_cmd = lc(pop(@{$$self{command_stack}}));
+		if (defined $prev_cmd and $prev_cmd ne '') 
 		{
+			&::print_log("PLM: Defined:$prev_cmd");
 			#put the command back into the stack.. Its not our job to tamper with this array
 			push(@{$$self{command_stack}},$prev_cmd);
 		}
-		$prev_cmd = unpack("H*",$prev_cmd);
-		&::print_log("PLM: Prev command:$prev_cmd:");		
+		&::print_log("PLM: Current Command:$data_1: Prev command:$prev_cmd:". length($prev_cmd) . ":");		
 
 		#check to see if this is a ack/err from a previous command
 		if ($prev_cmd ne '' and substr($data_1,0,length($prev_cmd)) eq $prev_cmd) 
@@ -400,6 +394,11 @@ sub _parse_data {
 		} elsif (substr($data_1,0,4) eq '0252') { #X10 Received
 			&::print_log("X10 Received:$data_1");
 			&::process_serial_data($self->_xlate_x10_mh($data_1));	
+		} elsif (substr($data_1,0,2) eq '15') { #X10 Received
+			&::print_log("PLM Interface extremely busy.");
+			#retry
+			$$self{xmit_in_progress} = 0;
+			$self->process_command_stack();			
 		} else {
 			#for now anything not recognized, kill pending xmission
 			$$self{xmit_in_progress} = 0;
@@ -434,13 +433,6 @@ sub _xlate_mh_x10
         $cmd=lc($cmd);
 
 	my $id=lc($p_setby->{id_by_state}{$cmd});
-    &::print_log("PLM: XLATE IDbyState:$cmd:$id:");
-
-#	if ($x10_commands{$cmd} eq undef) 
-#	{
-#		&::print_log("PLM: Object:$p_setby, Command $cmd not found.");
-#		return undef;
-#	}
 
 	my $hc = lc(substr($p_setby->{x10_id},1,1));
 	my $uc = lc(substr($p_setby->{x10_id},2,1));
@@ -449,8 +441,6 @@ sub _xlate_mh_x10
 		&::print_log("PLM: Object:$p_setby Doesnt have an x10 id (yet)");
 		return undef;
 	}
-#   &::print_log("XLATE:$cmd:$p_state:psb:$p_setby:id:" . $p_setby->{x10_id} . ":" . $p_setby->{id} . ":");
-#	$cmd=substr(unpack("H*",$x10_commands{$cmd}),2,1);
 
 	#Every X10 message starts with the House and unit code
 	$msg = "02";
@@ -458,7 +448,6 @@ sub _xlate_mh_x10
 	$msg.= substr(unpack("H*",pack("C",$x10_house_codes{substr($id,1,1)})),1,1);
 	$msg.= substr(unpack("H*",pack("C",$x10_unit_codes{substr($id,2,1)})),1,1);
 	$msg.= "00";
-	$msg=pack("H*",$msg);
 	$self->send_plm_cmd($msg);
 
 	my $ecmd;
@@ -480,7 +469,6 @@ sub _xlate_mh_x10
 			$msg.= substr(unpack("H*",pack("C",$x10_commands{substr($id,$pos,1)})),1,1);			
 		}
 		$msg.= "80";
-		$msg=pack("H*",$msg);
 		$self->send_plm_cmd($msg);
 	}
 }
@@ -495,9 +483,10 @@ sub _xlate_x10_mh
 	$msg.= uc($mh_unit_codes{substr($data,5,1)});
 &::print_log("PLM: XMH:$data:$msg:");
 	
-	for (my $index =6; $index<length($data)-2; $index++)
+	for (my $index =6; $index<length($data)-2; $index+=2)
 	{
-	    $msg.= uc($mh_house_codes{substr($data,$index,1)});
+            $msg.= uc($mh_house_codes{substr($data,$index,1)});
+	    $msg.= uc($mh_commands{substr($data,$index+1,1)});
 	}
 
 &::print_log("PLM:2XMH:$data:$msg:");
