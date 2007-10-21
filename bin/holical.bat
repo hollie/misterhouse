e787015a03cc3259199ac7a471e60646bcbd5891
hplato <hplato@71d350d0-f70e-0410-9bbe-9702f904aa17>
2007-10-21 22:31:16 +0000

    iCal based Holiday generation code for different regions. Regional holidays are specified in data/holidays.* and an apple ical/ms outlook compatible ical file will be created at the root of the web server. This file can be imported into a calendar tool or imported back into MH via the organizer ical2vsdb parser. Setting that file's option to holiday enables the organizer.pl's holiday functionality.
    Lots of dependencies, so if anyone wants to test this out see the requirements in bin/holical. If this is useful system-wide, it may be worthwhile to add the dependancies to lib/site (similar to what was done for ical2vsdb)

---
+++ bin/holical.bat
@@ -0,0 +1,2 @@
+@mh -run holical %1 %2 %3
+@rem perl -S holical %1 %2 %3
---
+++ code/common/holiday_ical.pl
@@ -0,0 +1,52 @@
+# Category = Time
+
+#@ Generate ical compatible holiday information every year
+#@ Can be used with ical2vsdb to reimport holiday information back into MH
+
+=begin comment
+
+mh.ini parameters required
+
+holiday_definition_file = path to filename that has holiday definitions
+ie holiday_definition_file = $Pgm_Root/data/holidays.ca_ab for Alberta, Canada
+
+optional
+
+   holiday_ical_filename = filename of generated holiday information (defaults to $Pgm_Root/web/holical.ics)
+   holiday_no_stats = 1 to not generate stat holidays (next business day if the holiday is on a weekend)
+
+-------------------------------------------------------------------------------------
+Note: $Pgm_Root/bin/holical (ical creator helper program) has several dependencies:
+
+Date::Easter, Date::Manip, Date::ICal, Data::ICal, Data::ICal::Entry::Event
+-------------------------------------------------------------------------------------
+
+=cut
+
+
+$p_holical = new Process_Item();
+
+$v_generate_holidays = new Voice_Cmd('Generate holiday information for [1,2,3,4] year(s)');
+$v_generate_holidays->set_info('Generate ical compatible holiday information');
+$v_generate_holidays->set_authority('anyone');
+my $holiday_output_filename = $config_parms{holiday_ical_filename};
+$holiday_output_filename = "$Pgm_Root/web/holical.ics" if !$holiday_output_filename;
+
+
+if ((said $v_generate_holidays) or ($New_Year) or
+    (($Reload) and !(-e $holiday_output_filename))) {
+
+	print_log "MH_Holidays: Starting holiday generation process...";
+	my $options;
+	$options = "-n" if $config_parms{holiday_no_stats};
+	my $years;
+	$years = said $v_generate_holidays;
+        $years = "0" if !$years;
+
+	if (-e $config_parms{holiday_definition_file}) {
+	  $p_holical->set("holical -y +$years $options -f $holiday_output_filename $config_parms{holiday_definition_file} ");
+	  start $p_holical;
+	} else {
+	  print_log "MH_Holidays: Error, cannot find holiday definition file!";
+	}
+}
