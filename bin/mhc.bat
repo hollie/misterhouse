b0ba9309195bb032dc827e07258497cde73c96e4
winter <winter@71d350d0-f70e-0410-9bbe-9702f904aa17>
2007-11-05 01:39:57 +0000

    Updated docs for 2.104 release.  Added bin/authors_svn

--- bin/mhc.bat
+++ bin/mhc.bat
@@ -12,6 +12,8 @@ set myM=%myM% -M DB_File -M DBI -M DBD::mysql -M DBD::Sponge -M Math::Trig -M Ne
 set myM=%myM% -M Tk::Text -M Tk::Menubutton -M Tk::Radiobutton -M Tk::JPEG -M Tk::CursorControl -M Tk::Checkbutton
 set myM=%myM% -M Net:::Jabber::Protocol -M XML::Stream::Node -M Net::Jabber -M Net::Jabber::Client -M CGI
 set myM=%myM% -M Time::localtime -M XML::RSS -M XML::Parser -M Crypt::SSLeay -M IPC::Open2 -M Encode
+set myM=%myM% -M Getopt::Long -M Text::Wrap -M Win32::Console -M Win32::WinError -M Win32::Sound -M GD 
+set myM=%myM% -M Win32::Registry -M Sys::Hostname -M Tk::BrowseEntry -M Tk::ProgressBar -M Tk::ToolBar
 
 @rem set myM=%myM% -M Audio::Mixer  -M Term::ReadKey -M Text::PhraseDistance
 
@@ -26,7 +28,7 @@ set myL=%myL% -l c:/perl/perl/bin/ssleay32.dll -l c:/perl/perl/bin/libeay32.dll
 @rem set PATH=c:\perl\perl\bin;%PATH%
 
 del mhc.errata
-call pp.bat -v 3 -i \compile\mh.ico -L mhc.errata %myM% %myL% -o mhen.exe  mh
+call pp.bat -v 3 -i ..\web\favicon.ico -L mhc.errata %myM% %myL% -o mhen.exe  mh
 
 echo done
 house speak done with compile
