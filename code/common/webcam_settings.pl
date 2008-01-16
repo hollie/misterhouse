1490b0060c788e91cf78bdcb3bb164e6533d4113
pjf02536 <pjf02536@71d350d0-f70e-0410-9bbe-9702f904aa17>
2008-01-16 02:40:13 +0000

    Adding webcam functioality and settings

---
+++ code/common/webcam_settings.pl
@@ -0,0 +1,27 @@
+
+# Category=Security
+
+#@ This script is a reference holder to access the Webcam parameters
+#@  <br>
+#@  <br>These are the settings for the Webcams mh.ini:/mh.private.ini file
+
+#@<table cellSpacing=0 cellPadding=2 border=1>
+#@<tr><th>
+#@</th><th>If these settings aren't in the mh.ini the default is the value shown BOLD</th></tr>
+#@<tr><td>Max Cam windows</td><td>wc_max=<b>4</b> &nbsp total number of cameras</td></tr>
+#@<tr><td>web address <br> for each cam [x]<br>up to wc_max</td>
+#@  <td>wc_address_[x]=http://webcam.ip.address:PORT/dir/filename.jpg,DESCTIPTION<br>
+#@  Description will be the text tag for the image frames<br>
+#@  Cameras are numbered from 1 to whatever</td></tr>
+#@<tr><td>Storage location for<br>webcam images</td><td>wc_slide_dir=<b>/cameras/cams</b></td></tr>
+#@</table>
+
+my $wc_max			= $config_parms{wc_max};
+
+my $wc_address_1		= $config_parms{wc_address_1};
+my $wc_address_2		= $config_parms{wc_address_2};
+my $wc_address_3		= $config_parms{wc_address_3};
+my $wc_address_4		= $config_parms{wc_address_4};
+
+#my $wc_slide_dir		= $config_parms{wc_slide_dir};
+#my $wc_bg_color			= $config_parms{wc_bg_color};
