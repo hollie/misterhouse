38e9e9225b858c1d75c77835013f76a5126e379b
gliming <gliming@71d350d0-f70e-0410-9bbe-9702f904aa17>
2008-01-28 21:37:26 +0000

    Implemented queued linking; improved sync link processing by only updating changes.

--- lib/Insteon_Link.pm
+++ lib/Insteon_Link.pm
@@ -65,9 +65,22 @@ sub add
         }
 }
 
+sub is_plm_controlled
+{
+	my ($self) = @_;
+	return $self->device_id eq '000000';
+}
+
+
 sub sync_links
 {
 	my ($self) = @_;
+	@{$$self{sync_queue}} = (); # reset the work queue
+	my $insteon_object = $self->interface;
+	if (!($self->is_plm_controlled)) {
+		$insteon_object = $self->interface->get_object($self->device_id,'01');
+	}
+	my $self_link_name = $self->get_object_name;
 	if ($$self{members}) {
 		foreach my $member_ref (keys %{$$self{members}}) {
 			my $member = $$self{members}{$member_ref}{object};
@@ -75,29 +88,103 @@ sub sync_links
 				my @children = $member->find_members('Insteon_Device');
 				$member = $children[0];
 			}
-			my $insteon_object = $self->interface;
-			if ($self->device_id ne '000000') {
-				$insteon_object = $self->interface()->get_object($self->device_id);
-			}
 			my $tgt_on_level = $$self{members}{$member_ref}{on_level};
+			$tgt_on_level = '100%' unless defined $tgt_on_level;
+
 			my $tgt_ramp_rate = $$self{members}{$member_ref}{ramp_rate};
+			$tgt_ramp_rate = '0' unless defined $tgt_ramp_rate;
 			# first, check existance for each link; if found, then perform an update (unless link is to PLM)
 			# if not, then add the link
 			if ($member->has_link($insteon_object, $self->group, 0)) {
 				# TO-DO: only update link if the on_level and ramp_rate are different
-				$member->update_link(object => $insteon_object, group => $self->group, is_controller => 0, 
-					on_level => $tgt_on_level, ramp_rate => $tgt_ramp_rate);
+				my $requires_update = 0;
+				$tgt_on_level =~ s/(\d+)%?/$1/;
+				$tgt_ramp_rate =~ s/(\d)s?/$1/;
+				my $adlbkey = lc $insteon_object->device_id . $self->group . '0';
+				if (!($member->is_dimmable)) {
+					if ($tgt_on_level >= 1 and $$member{adlb}{$adlbkey}{data1} ne 'ff') {
+						$requires_update = 1;
+						$tgt_on_level = 100;
+					} elsif ($tgt_on_level == 0 and $$member{adlb}{$adlbkey}{data1} ne '00') {
+						$requires_update = 1;
+					}
+					if ($$member{adlb}{$adlbkey}{data2} ne '00') {
+						$tgt_ramp_rate = 0;
+					}
+				} else {
+					$tgt_ramp_rate = 0.1 unless $tgt_ramp_rate;
+					my $link_on_level = hex($$member{adlb}{$adlbkey}{data1})/2.55;
+					my $raw_ramp_rate = $$member{adlb}{$adlbkey}{data2};
+					my $link_ramp_rate = &Insteon_Device::get_ramp_from_code($raw_ramp_rate);
+#					&::print_log("[Insteon_Link] " . $self->get_object_name . ": " . $member->get_object_name 
+#						. " tgt_on_level=$tgt_on_level, link_on_level=$link_on_level, "
+#						. "tgt_ramp_rate=$tgt_ramp_rate, link_ramp_rate=$link_ramp_rate");
+					if ($link_ramp_rate != $tgt_ramp_rate) {
+						$requires_update = 1;
+					} elsif (($link_on_level > $tgt_on_level + 1) or ($link_on_level < $tgt_on_level -1)) {
+						$requires_update = 1;
+					}
+				}
+				if ($requires_update) {
+					my %link_req = ( member => $member, cmd => 'update', object => $insteon_object, 
+						group => $self->group, is_controller => 0, 
+						on_level => $tgt_on_level, ramp_rate => $tgt_ramp_rate,
+						callback => "$self_link_name->_process_sync_queue()" );
+					push @{$$self{sync_queue}}, \%link_req;
+				}
 			} else {
-				$member->add_link(object => $insteon_object, group => $self->group, is_controller => 0);
+				my %link_req = ( member => $member, cmd => 'add', object => $insteon_object, 
+					group => $self->group, is_controller => 0, 
+					on_level => $tgt_on_level, ramp_rate => $tgt_ramp_rate,
+					callback => "$self_link_name->_process_sync_queue()" );
+				push @{$$self{sync_queue}}, \%link_req;
 			}
 			if (!($insteon_object->has_link($member, $self->group, 1))) {
-				$insteon_object->add_link(object => $member, group => $self->group, is_controller => 1);
+				my %link_req = ( member => $insteon_object, cmd => 'add', object => $member, 
+					group => $self->group, is_controller => 1, 
+					callback => "$self_link_name->_process_sync_queue()" );
+				push @{$$self{sync_queue}}, \%link_req;
 			}
 		}
 	}
+	# if not a plm controlled link, then confirm that a link back to the plm exists
+	if (!($self->is_plm_controlled)) {
+		if (!($insteon_object->has_link($self->interface,'01',1))) {
+			my %link_req = ( member => $insteon_object, cmd => 'add', object => $self->interface, 
+				group => '01', is_controller => 1, 
+				callback => "$self_link_name->_process_sync_queue()" );
+			push @{$$self{sync_queue}}, \%link_req;
+		}
+		if (!($self->interface->has_link($insteon_object,'01',0))) {
+			my %link_req = ( member => $self->interface, cmd => 'add', object => $insteon_object, 
+				group => '01', is_controller => 0, 
+				callback => "$self_link_name->_process_sync_queue()" );
+			push @{$$self{sync_queue}}, \%link_req;
+		}
+	}
+	if (@{$$self{sync_queue}}) {
+		$self->_process_sync_queue();
+	}
+	
 	# TO-DO: consult links table to determine if any "orphaned links" refer to this device; if so, then delete
 	# WARN: can't immediately do this as the link tables aren't finalized on the above operations
 	#    until the end of the actual insteon memory poke sequences; therefore, may need to handle separately
+}
+
+sub _process_sync_queue {
+	my ($self) = @_;
+	# get next in queue if it exists
+	if (@{$$self{sync_queue}}) {
+		my $link_req_ptr = shift(@{$$self{sync_queue}});
+		my %link_req = %$link_req_ptr;
+		if ($link_req{cmd} eq 'update') {
+			my $link_member = $link_req{member};
+			$link_member->update_link(%link_req);
+		} elsif ($link_req{cmd} eq 'add') {
+			my $link_member = $link_req{member};
+			$link_member->add_link(%link_req);
+		} 
+	}
 }
 
 sub set
