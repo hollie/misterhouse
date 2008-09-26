212b1b402ce195756dd4790082d93a3044b8c36b
gliming <gliming@71d350d0-f70e-0410-9bbe-9702f904aa17>
2008-09-26 15:00:18 +0000

    Rewrote logic to track timing of each received message and to use this info to guard against "hangs".

--- lib/X10_W800.pm
+++ lib/X10_W800.pm
@@ -46,122 +46,202 @@ sub startup {
    &::MainLoop_pre_add_hook(  \&X10_W800::check_for_data, 1 ) if $main::Serial_Ports{W800}{object};
 }
 
-my ($prev_data, $prev_time, $prev_loop, $prev_done, $prev_bad_checksums);
-$prev_data = $prev_time = $prev_loop = $prev_done = $prev_bad_checksums = 0;
+my ($prev_bad_checksums);
+$prev_bad_checksums = 0;
 
 my ($new_data_time);	# Leave undefined initially
+my ($prev_residual, @msg_buffer, $reset_prev_residual_pending);
+$prev_residual = '';
+$reset_prev_residual_pending = 0; # don't reset it
+@msg_buffer = ();
 
 sub check_for_data {
     my ($self) = @_;
     &main::check_for_generic_serial_data('W800');
 
-    # Nothing to do if there is nothing in the buffer.
+   # Nothing to do if there is nothing in the buffer.
     unless ($main::Serial_Ports{W800}{data}) {
-	if ($prev_bad_checksums != 0) {
-	    &::print_log(  "W800: failed to recovered from bad checksums "
-		         . "(count=$prev_bad_checksums)");
-	    $prev_bad_checksums = 0;
-	}
+       if ($prev_bad_checksums != 0 and !($prev_residual)) {
+           &::print_log(  "W800: failed to recover from bad checksums due to no data"
+               . "(count=$prev_bad_checksums)");
+           $prev_bad_checksums = 0;
+        }
 
-	$new_data_time = undef;
+        $new_data_time = undef;
 
-	return;
+        return;
     }
+
+    my $buffer = $main::Serial_Ports{W800}{data};
+    if ($buffer and $prev_residual) {
+       # mark prev_residual as being ready for reset if conditions below warrant it
+       $reset_prev_residual_pending = 1;
+    }
+    $buffer = $prev_residual . $buffer if defined $prev_residual;
+
+    # clear out the serial port's data
+    $main::Serial_Ports{W800}{data} = undef;
 
     # Get a current time reading to be used with a couple of timers.
     my $time = &main::get_tickcount;
-
-    # If this is the first signs of new data in a while, make a note of the
-    # time.
-    $new_data_time = $time unless defined $new_data_time;
-
-    # See if we have at least 4 bytes in the buffer.  (allow for 0x0A with /s)
-    my ($data, $remainder) = $main::Serial_Ports{W800}{data} =~ /(....)(.*)/s;
+    $time += 2**32 if $time < 0; # force a wrap if mh's tickcount wrapped negative
 
     # A valid command is 4 bytes long.  If we didn't get 4 bytes from the
     # serial stream, then we are not ready to process a command.
-    unless (defined $data) {
-	# If it's been a while since the time the we started processing the
-	# data stream and we still don't have enough bytes to finish the
-	# command, flush the data buffer since it probably contains left over
-	# data from a corrupt data stream.
-	# NOTE: get_tickcount wraps, so $time < $new_data_time test is to
-	#       make sure that doesn't become a problem.
-	if ($time > $new_data_time + 2000 or $time < $new_data_time) {
-	    my $hex = unpack "H*", $main::Serial_Ports{W800}{data};
-	    &::print_log("W800: flushing incomplete data: $hex");
+    unless (defined $buffer and length($buffer) >= 4) {
+       # If it's been a while since the time the we started processing the
+       # data stream and we still don't have enough bytes to finish the
+       # command, flush the data buffer since it probably contains left over
+       # data from a corrupt data stream.
+       # NOTE: get_tickcount wraps, so $time < $new_data_time test is to
+       #       make sure that doesn't become a problem.
+       if (&X10_W800::is_within_timeout($time, $new_data_time, 2000)) {
+          my $hex = unpack "H*", $main::Serial_Ports{W800}{data};
+          &::print_log("W800: flushing incomplete data: $hex");
 
-	    if ($prev_bad_checksums != 0) {
-		&::print_log(  "W800: failed to recovered from bad checksums "
-			     . "(count=$prev_bad_checksums)");
-		$prev_bad_checksums = 0;
-	    }
+          if ($prev_bad_checksums != 0) {
+             &::print_log(  "W800: failed to recover from bad checksums "
+                 . "(count=$prev_bad_checksums)");
+             $prev_bad_checksums = 0;
+          }
 
-	    $main::Serial_Ports{W800}{data} = undef;
-	    $new_data_time = undef;
-	}
-	return;
+          $new_data_time = undef;
+       }
+       return;
     }
 
-    # Got 4 bytes.  We'll keep them and put the remainder back in the buffer.
-    $main::Serial_Ports{W800}{data} = $remainder;
+    # See if we have at least 4 bytes in the buffer.  (allow for 0x0A with /s)
+    my ($data, $residual) = $buffer =~ /(....)(.*)/s;
 
     # Even if there is some data left in the buffer we'll reset the time the
     # data stream started so that the next series of bytes will have a
     # reasonable time complete.
     $new_data_time = undef;
 
-    # Data gets sent multiple times
-    #  - Check time and loop count.  If mh paused (e.g. sending ir data)
-    #    then we better also check loop count.
-    #  - Process data only on the 2nd occurance, to avoid noise
-    my $repeat_time = $main::config_parms{W800_multireceive_delay} || 1500;
-    my $repeat_data =    ($data eq $prev_data)
-		      && (   $time < $prev_time + $repeat_time
-			  || $main::Loop_Count < $prev_loop + 7);
-    return if $repeat_data and $prev_done;
-    $prev_data = $data;
-    $prev_time = $time;
-    $prev_loop = $main::Loop_Count;
-    unless ($repeat_data) {     # UnSet flag and wait for 2nd occurance
-	$prev_done = 0;
-	return;
+
+   while ($data) {
+
+      # Got 4 bytes.  We'll keep them and put the residual back in the buffer.
+
+      # Data gets sent multiple times
+      #  - Check time
+      #  - Process data only on the 2nd occurance, to avoid noise (seems essential)
+      my $duplicate_threshold = 1; # 2nd occurance; set to 0 to omit duplicate check
+      if (&X10_W800::duplicate_count($data) == $duplicate_threshold) {
+
+         # If this is the first signs of new data in a while, make a note of the
+         # time.
+         $new_data_time = $time unless defined $new_data_time;
+
+         # Log the raw data.
+         my $hex = unpack "H*", $data;
+         &::print_log("W800: raw data: $hex") if $main::Debug{w800};
+
+         # For powerline type data bytes 1 and 3 contain the real data.  Bytes 2 and
+         # 4 are just complements of bytes 1 and 2 (for doing checksums).  XOR'ing
+         # byte 1 with byte 2 should result in 0xff.  The same goes for bytes 3 and
+         # 4.  For security data, byte 4 is not a checksum and does contain part of
+         # the device ID.  Other devices, such as the Digimax 210, don't seem to
+         # have checksums at all.  See X10_RF.pm for more details.
+         my @bytes = $data =~ /^(.)(.)(.)(.)$/s;
+
+         my $state = X10_RF::decode_rf_bytes('w800', @bytes);
+
+         # If the decode_rf_bytes routine didn't like the data that it got,
+         # strip the first byte off and let the rest be resubmitted in case a
+         # rogue byte got inserted into the data stream.
+         if ($state eq 'BADCHECKSUM') {
+
+             if ($prev_bad_checksums == 0) {
+                &::print_log("W800: bad checksum (attempting to recover)");
+             }
+             $prev_bad_checksums++;
+             $data = substr($data,1) if length($data) > 1;
+             $buffer = $data . $residual;
+          } else {
+             # we have "good" (not BADCHECKSUM) data
+
+             # Report if we recovered from previous bad checksums.
+             if ($prev_bad_checksums != 0) {
+                &::print_log(  "W800: recovered from bad checksum "
+                    . "(count=$prev_bad_checksums)");
+             }
+             # reset bad checksum counter since this one is ok
+             $prev_bad_checksums = 0;
+             # set buffer to any remaining bytes in case they can be processed
+             $buffer = $residual;
+          }
+      } else {
+          $buffer = $residual; # in case there's any residual
+#          $prev_bad_checksums = 0;
+      }
+      ($data, $residual) = $buffer =~ /(....)(.*)/s;
+       # set residual to buffer if no data present
+       $residual = $buffer unless defined $data;
     }
-    $prev_done = 1;             # Set flag and process data
 
-    # Log the raw data.
-    my $hex = unpack "H*", $data;
-    &::print_log("W800: raw data: $hex") if $main::Debug{w800};
-
-    # For powerline type data bytes 1 and 3 contain the real data.  Bytes 2 and
-    # 4 are just complements of bytes 1 and 2 (for doing checksums).  XOR'ing
-    # byte 1 with byte 2 should result in 0xff.  The same goes for bytes 3 and
-    # 4.  For security data, byte 4 is not a checksum and does contain part of
-    # the device ID.  Other devices, such as the Digimax 210, don't seem to
-    # have checksums at all.  See X10_RF.pm for more details.
-    my @bytes = $data =~ /^(.)(.)(.)(.)$/s;
-
-    my $state = X10_RF::decode_rf_bytes('w800', @bytes);
-
-    # If the decode_rf_bytes routine didn't like the data that it got,
-    # strip the first byte off and let the rest be resubmitted in case a
-    # rogue byte got inserted into the data stream.
-    if ($state eq 'BADCHECKSUM') {
-	$data =~ s/^.//;
-	$main::Serial_Ports{W800}{data} = $data.$main::Serial_Ports{W800}{data};
-
-	if ($prev_bad_checksums == 0) {
-	    &::print_log("W800: bad checksum (attempting to recover)");
-	}
-	$prev_bad_checksums++;
+    if ($prev_residual eq $residual and $reset_prev_residual_pending) {
+       # don't keep it around as we have seen it before and need to get rid of it
+       $prev_residual = '';
     } else {
-	# Report if we recovered from previous bad checksums.
-	if ($prev_bad_checksums != 0) {
-	    &::print_log(  "W800: recovered from bad checksum "
-		         . "(count=$prev_bad_checksums)");
-	}
-	$prev_bad_checksums = 0;
+       # keep a copy for the next time that we get data
+       $prev_residual = $residual;
     }
+    $reset_prev_residual_pending = 0; # always clear the reset flag since 
+                                      # it should only be reset on new data and prev_residual
+
+}
+
+sub duplicate_count
+{
+    my ($raw_msg) = @_;
+    my $duplicate_count = 0;
+    my $repeat_time = $main::config_parms{W800_multireceive_delay} || 1500;
+    my $time = &main::get_tickcount;
+
+    if (my $msg_buffer_size = @msg_buffer) {
+     # most recent messages are always first in the queue
+       for my $msg_ptr (@msg_buffer) {
+          my %msg = %$msg_ptr;
+          if  (&X10_W800::is_within_timeout($time, $msg{time}, $repeat_time)) {
+             # a match exists on the data; so, compare against the time stamp
+             if ($raw_msg eq $msg{data}) {
+                # it's a duplicate since it's within the multireceive_delay
+                $duplicate_count++;
+             }
+          } else {
+             # no point in continueing to look at the rest as it's out of the time window
+             last;
+          }
+       }
+    }
+    # add to the msg buffer if it is not a duplicate
+    &X10_W800::add_data($raw_msg) unless $duplicate_count > 2;
+    return $duplicate_count;
+}
+
+sub add_data
+{
+    my ($data) = @_;
+    my %msg = {};
+    $msg{data} = $data;
+    $msg{time} = &main::get_tickcount;
+
+    my $max_length = 20;
+    my $msg_buffer_size = @msg_buffer;
+    while ($msg_buffer_size >= $max_length) {
+    # most recent messages are always first in the queue
+       pop @msg_buffer;
+       $msg_buffer_size = @msg_buffer;
+    }
+    unshift @msg_buffer, \%msg;
+}
+
+sub is_within_timeout
+{
+    my ($time1, $time2, $timeout) = @_;
+    return 1 if (($time1 >= 2**7) and ($time2 < 2**7));
+    return ($time1 < ($time2 + $timeout)) ? 1 : 0;
 }
 
 # Data format info on here:  http://www.wgldesigns.com/dataformat.txt
