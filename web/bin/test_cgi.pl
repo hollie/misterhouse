01d07de26929cca0c794a20434e0ed7f82607804
dnorwood2 <dnorwood2@71d350d0-f70e-0410-9bbe-9702f904aa17>
2008-09-30 10:17:56 +0000

    added debugging information

--- web/bin/test_cgi.pl
+++ web/bin/test_cgi.pl
@@ -1,5 +1,34 @@
-#!/local/bin/perl 
+#!/usr/bin/perl 
 
 print "Content-Type: text/html\n\n";
+
+print "<html><body>\n";
 print "<h2>Hello World from mh test_cgi.pl</h2>";
 
+print "<p/><h4>POST Data:</h4>\n<p>";
+while (my ($key, $value) = each %HTTP_ARGV){
+	print "$key=$value<br>\n";
+}
+print "</p>\n";
+
+print "<p/><h4>Query Data:</h4>\n<p>";
+while (my ($key, $value) = each %Http){
+	print "$key=$value<br>\n";
+}
+print "</p>\n";
+
+#print "<p/><h4>HTTP Content:</h4>\n<p>$HTTP_CONTENT</p>\n\n";
+
+open E, "> /tmp/e";
+#print E $HTTP_CONTENT;
+close E;
+
+
+print "<h4>stdin\n<pre>\n";
+while (<STDIN>) {
+	print "$_\n";
+}
+		
+print "</pre>\n";
+	
+print "</body></html>\n";
