e49943e5e9b160285905df457557f282bbe46e78
dnorwood2 <dnorwood2@71d350d0-f70e-0410-9bbe-9702f904aa17>
2009-08-28 21:50:36 +0000

    I have created 3 web pages that demonstrate using AJAX technology with Misterhouse.  The easiest way to view them is the do an svn update, then point your browser at http://localhost:8080/test, and then click on one of the ajax examples from the list.  To get this working, I made substantial changes to the Misterhouse XML server.  I think my changes are backwards compatible, and hope I didn't break anything.  I also started a default xsl style sheet for formatting the xml data.  This needs a lot more work, and any input from someone who knows what he/she is doing would be appreciated.  There should also be a way to override the default xsl style sheet (maybe a parameter passed to the xml subroutine).  I have an idea how to modify the Misterhouse http server to support Comet technoiogy.  This would be much more efficient than the polling technique used in these examples.  Contact me if you are interested in this.

--- lib/xml_server.pl
+++ lib/xml_server.pl
@@ -11,88 +11,98 @@ use strict;
 # Called via the web server.  If no request, xml for all types is returned.  Examples:
 #   http://localhost:8080/sub?xml
 #   http://localhost:8080/sub?xml(vars)
+# You can also specify which objects, groups, categories, variables, etc to return (by default, all) Example: 
+#   http://me:8080/sub?xml(weather=TempIndoor|TempOutdoor)
+# You can also specify which fields of objects are returned (by default, all) Example: 
+#   http://localhost:8080/sub?xml(groups=$All_Lights,fields=html)
+
+# TODO 
+# The = operator isn't supported on all data types 
+# There should be a usage data type that gives clickable examples 
+# There should be a way to override the default xsl file 
 
 use HTML::Entities; # So we can encode characters like <>& etc
  
-sub xml {
+sub xml {		
 	my ($request, $options) = @_;
 	my ($xml, $xml_types, $xml_groups, $xml_categories, $xml_widgets, $xml_vars, $xml_objects);
 
 	$request = 'types,groups,categories,widgets,config_parms,weather,save,vars,objects' unless $request;
-	my %request = map {$_, 1} split ',', $request;
+	my %request;
+	foreach (split ',', $request) {
+		my ($k, undef, $v) = /(\w+)(=([\w\|\$]+))?/;
+		$request{$k}{active} = 1;
+		$request{$k}{members} = [ split /\|/, $v ] if $k and $v;
+	}
 	  
-	my ($show_type,$show_group); 
-	($show_type) = $options =~ /type=(\w+)/;
-	($show_group) = $options =~ /group=([\$_\w]+)/;
+	my %options;
+	foreach (split ',', $options) {
+		my ($k, undef, $v) = /(\w+)(=([\w\|\_]+))?/;
+		$options{$k}{active} = 1;
+		$options{$k}{members} = [ split /\|/, $v ] if $k and $v;
+	}
+	  
+	my %fields; 
+	if (exists $options{fields}{members}) {
+		foreach (@{ $options{fields}{members} }) {
+			$fields{$_} = 1;
+		}
+	}
 
 			# List objects by type
 	if ($request{types}) {
-		for my $object_type (@Object_Types) {
-			if (my @members = sort &list_objects_by_type($object_type)) {
-				$xml_types .= "<object_type>\n  <name> $object_type</name>\n";
-				$xml_types .= "  <members>@members</members>\n";
-				$xml_types .= "</object_type>\n";
+		$xml .= "\t<types>\n";
+		for my $object_type (sort @Object_Types) {
+			next if exists $request{types}{members} and (not grep {$_ eq $object_type} @{ $request{types}{members} });
+			$xml .= "\t\t<type>\n\t\t\t<name>$object_type</name>\n";
+			foreach (sort &list_objects_by_type($object_type)) {	
+				$_ = &get_object_by_name($_);
+				$xml .= &object_detail($_, %fields);
 			}
+			$xml .= "\t\t</type>\n";
 		}
-		$xml .= "<object_types>\n$xml_types</object_types>\n";
+		$xml .= "\t</types>\n";
 	}
 
 			# List objects by groups
 	if ($request{groups}) {
-		for my $group (&list_objects_by_type('Group')) {
-			my $object = &get_object_by_name($group);
-			if (my @members = list $object) {
-				@members = map{$_->{object_name}} @members;
-				$xml_groups .= "<group>\n  <name>$group</name>\n";
-				$xml_groups .= "  <members>@members</members>\n";
-				$xml_groups .= "</group>\n";
+		$xml .= "\t<groups>\n";
+		for my $group (sort &list_objects_by_type('Group')) {
+			next if exists $request{groups}{members} and (not grep {$_ eq $group} @{ $request{groups}{members} });
+			my $group_object = &get_object_by_name($group);
+			$xml .= "\t\t<group>\n\t\t\t<name>$group</name>\n";
+			foreach (list $group_object) {
+				$xml .= &object_detail($_, %fields);
 			}
+			$xml .= "\t\t</group>\n";
 		}
-		$xml .= "<groups>\n$xml_groups</groups>\n";
+		$xml .= "\t</groups>\n";
 	}
 
-			# List objects by category
+			# List voice commands by category
 	if ($request{categories}){
-		for my $category (&list_code_webnames) {
-			if (my @members = &list_objects_by_webname($category)) {
-				$xml_categories .= "<category>\n  <name>$category</name>\n";
-				$xml_categories .= "  <members>@members</members>\n";
-				$xml_categories .= "</category>\n";
+		$xml .= "\t<categories>\n";
+		for my $category (&list_code_webnames('Voice_Cmd')) {
+			next if $category =~ /^none$/;
+			next if exists $request{categories}{members} and (not grep {$_ eq $category} @{ $request{categories}{members} });
+			$xml .= "\t\t<category>\n\t\t\t<name>$category</name>\n";
+			foreach (sort &list_objects_by_webname($category)) {
+				$_ = &get_object_by_name($_);
+				$xml .= &object_detail($_, %fields);
 			}
+			$xml .= "\t\t</category>\n";
 		}
-		$xml .= "<categories>\n$xml_categories</categories>\n";
+		$xml .= "\t</categories>\n";
 	}		
 
 			# List objects
 	if ($request{objects}) {
-		my ($group_object, @group_members);
-		$group_object = &get_object_by_name($show_group) if $show_group;
-		@group_members  = map{$_->{object_name}} list $group_object if $group_object;
 		for my $object_type (@Object_Types) {
-			next if $show_type and ($object_type ne $show_type);
 			$xml_objects .= "  <object_type>\n	<name>$object_type</name>\n";
 			if (my @object_list = sort &list_objects_by_type($object_type)) {
-				for my $object (map{&get_object_by_name($_)} @object_list) {
+		for my $object (map{&get_object_by_name($_)} @object_list) {
 					next if $object->{hidden};
-					my $object_name = $object->{object_name};
-&::print_log($object->{object_name}, @group_members);
-					next if $show_group and (not grep /^$object_name$/, @group_members);
-					$xml_objects .= "	<object>\n";
-					$xml_objects .= "	  <name>$object_name</name>\n";
-					$xml_objects .= "	  <file>$object->{filename}</file>\n";
-					$xml_objects .= "	  <category>$object->{category}</category>\n";
-					my $state = encode_entities($object->{state}, "\200-\377&<>");
-					$xml_objects .= "	  <state>$state</state>\n";
-#					$xml_objects .= "	  <set_by>" . $object->get_set_by . "</set_by>\n" if defined &$object->get_set_by;
-					$xml_objects .= "	  <type>$object->{get_type}</type>\n";
-					$xml_objects .= "	  <states>@{$object->{states}}</states>\n" if $object->{states};
-					$xml_objects .= "	  <text>$object->{text}</text>\n" if $object->{text};
-					$xml_objects .= "	  <html><!\[CDATA\[\n" . &html_item_state($object, $object->{get_type}) . "\]\]>\n	  
-</html>\n";
-					if ($object_type eq 'Timer') {
-						$xml_objects .= "	  <seconds_remaining>" . $object->seconds_remaining . "</seconds_remaining>\n";
-					}
-					$xml_objects .= "	</object>\n";
+					$xml_objects .= &object_detail($object, %fields);
 				}
 			}
 			$xml_objects .= "  </object_type>\n";
@@ -166,7 +176,7 @@ sub xml {
 			}
 		}
 		$xml .= "  <vars>\n$xml_vars  </vars>\n";
-	} 
+		} 
 
 			# Translate special characters
 	$xml = encode_entities($xml, "\200-\377&");
@@ -176,12 +186,34 @@ sub xml {
 	return &xml_page($xml);
 }
 
+sub object_detail {
+	my ($object, %fields) = @_;
+	return if $fields{none};
+	my $object_name = $object->{object_name};
+	my $xml_objects  = "\t\t\t<object>\n";
+	$xml_objects .= "\t\t\t\t<name>$object_name</name>\n";
+	$xml_objects .= "\t\t\t\t<filename>$object->{filename}</file>\n" 			if $fields{all} or $fields{filename};
+	$xml_objects .= "\t\t\t\t<category>$object->{category}</category>\n"		if $fields{all} or $fields{category};
+	my $state = encode_entities($object->{state}, "\200-\377&<>");
+	$xml_objects .= "\t\t\t\t<state>$state</state>\n"					if $fields{all} or $fields{state};
+#	$xml_objects .= "\t\t\t\t<set_by>" . $object->get_set_by . "</set_by>\n" if defined &$object->get_set_by;
+	$xml_objects .= "\t\t\t\t<type>$object->{get_type}</type>\n"			if $fields{all} or $fields{type};
+	$xml_objects .= "\t\t\t\t<states>@{$object->{states}}</states>\n"			if $fields{all} or $fields{states};
+	$xml_objects .= "\t\t\t\t<text>$object->{text}</text>\n"				if $fields{all} or $fields{text};
+	$xml_objects .= "\t\t\t\t<html><!\[CDATA\[" . &html_item_state($object, $object->{get_type}) . "\]\]>\n\t\t\t\t</html>\n"
+														if $fields{all} or $fields{html};
+	if ($object->{get_type} eq 'Timer') {
+		$xml_objects .= "\t\t\t\t<seconds_remaining>" . $object->seconds_remaining . "</seconds_remaining>\n";
+	}
+	$xml_objects .= "\t\t\t</object>\n";
+	return $xml_objects; 
+}
+
 sub xml_page {
 	my ($xml) = @_;
 
 #<!DOCTYPE document SYSTEM "misterhouse.dtd">
 #<?xml version="1.0" standalone="no" ?>
-#<?xml-stylesheet type="text/xsl" href="simpletest.xsl"?>
 
 	return <<eof;
 HTTP/1.0 200 OK
@@ -189,10 +221,21 @@ Server: MisterHouse
 Content-type: text/xml
 
 <?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<?xml-stylesheet type="text/xsl" href="/default.xsl"?>
 $xml
 
 eof
 
+}
+
+sub xml_entities_encode { 
+	my $s = shift; 
+	$s =~ s/\&/&amp;/g;
+	$s =~ s/\</&lt;/g;
+	$s =~ s/\>/&gt;/g;
+	$s =~ s/\'/&apos;/g;
+	$s =~ s/\"/&quot;/g;
+	return $s;
 }
 
 sub svg_page {
