780ef23860a0d4ea0e99aac41cab3ae67428c7d4
marcmerlin <marcmerlin@71d350d0-f70e-0410-9bbe-9702f904aa17>
2011-08-01 01:13:40 +0000

    updated doc

--- lib/X10_CMxx.pm
+++ lib/X10_CMxx.pm
@@ -9,9 +9,7 @@ program, mochad, which writes it to a fifo which this module reads.
 1) Download and install mochad: http://sourceforge.net/projects/mochad/
 
  Make sure that your version supports the --raw-data option.
- If your mochad is too old (or a version newer than 0.1.7 has not been
- released yet), apply this patch to mochad: X10_CMxx_mochad-0.1.7.diff
- and see the README.
+ If your mochad is too old, upgrade to something newer than 0.1.7.
 
 2) Use these mh.ini parameters to enable this code:
 
