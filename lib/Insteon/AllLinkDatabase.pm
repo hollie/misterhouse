83cd3b280fcf0853620580dd61b463b0b875365c
KRKeegan <kevin@krkeegan.com>
2013-03-03 16:58:17 -0800

    Add ALDB_Delta support to add_link

--- lib/Insteon/AllLinkDatabase.pm
+++ lib/Insteon/AllLinkDatabase.pm
@@ -870,7 +870,12 @@ sub add_link
 {
 	my ($self, $parms_text) = @_;
 	my %link_parms;
-	if (@_ > 2)
+	if ($parms_text eq 'ok' or $parms_text eq 'fail'){
+		%link_parms = %{$self->{callback_parms}};
+		$$self{callback_parms} = undef;
+		$link_parms{aldb_check} = $parms_text;
+	}
+	elsif (@_ > 2)
         {
 		shift @_;
 		%link_parms = @_;
@@ -901,7 +906,24 @@ sub add_link
         {
 		$key .= $subaddress;
 	}
-	if (defined $$self{aldb}{$key} && defined $$self{aldb}{$key}{inuse})
+	if (!defined($link_parms{aldb_check}) && (!$$self{device}->isa('Insteon_PLM'))){
+		## Check whether ALDB is in sync
+		$self->{callback_parms} = \%link_parms;
+		$$self{_aldb_unchanged_callback} = '&Insteon::AllLinkDatabase::add_link('.$$self{device}->{object_name}."->_aldb, 'ok')";
+		$$self{_aldb_changed_callback} = '&Insteon::AllLinkDatabase::add_link('.$$self{device}->{object_name}."->_aldb, 'fail')";
+		$self->query_aldb_delta("check");
+	} elsif ($link_parms{aldb_check} eq "fail"){
+		&::print_log("[Insteon::AllLinkDatabase] WARN: Link NOT added, please rescan this device and sync again.");
+		if ($link_parms{callback})
+		{
+			package main;
+			eval($link_parms{callback});
+			&::print_log("[Insteon::AllLinkDatabase] failure occurred in callback eval for " . $$self{device}->get_object_name . ":" . $@)
+				if $@ and $main::Debug{insteon};
+			package Insteon::AllLinkDatabase;
+		}
+	}
+	elsif (defined $$self{aldb}{$key} && defined $$self{aldb}{$key}{inuse})
         {
 		&::print_log("[Insteon::AllLinkDatabase] WARN: attempt to add link to " 
 			. $$self{device}->get_object_name 
@@ -917,7 +939,7 @@ sub add_link
 			package Insteon::AllLinkDatabase;
 		}
 	}
-        else
+	elsif ($link_parms{aldb_check} eq "ok")
         {
 		# strip optional % sign to append on_level
 		my $on_level = $link_parms{on_level};
