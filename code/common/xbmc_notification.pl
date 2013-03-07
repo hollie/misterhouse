4af6b71b69f5059ff94e53926fdcdcb7a6f1c5a7
Ryan Davies <ryan@ryandavies.co.nz>
2013-03-07 21:39:52 +1300

    Refined XBMC Notifications

--- code/common/xbmc_notification.pl
+++ code/common/xbmc_notification.pl
@@ -14,23 +14,13 @@ if ($state = said $v_xbmc_osd) {
 	&display_xbmcosd("Test Notification", "This is a test notification!!");
 }
 
-
-
-$xbmc_osd = new  Socket_Item(undef, undef, $config_parms{xbmc_notify_address}, 'xbmc', 'tcp');
-
 sub display_xbmcosd {
-	my ($heading, $text) = @_;
+	my ($title, $text) = @_;
 
-	$heading =~ s/ /%20/g;
+	$title =~ s/ /%20/g;
 	$text =~ s/ /%20/g;
 
-	start $xbmc_osd unless (active $xbmc_osd);
-	set $xbmc_osd <<EOT;
-GET /jsonrpc?request={%22jsonrpc%22:%222.0%22,%22method%22:%22GUI.ShowNotification%22,%22params%22:{%22title%22:%22${heading}%22,%22message%22:%22${text}%22},%22id%22:1} HTTP/1.1
-Connection: close
-User-Agent: Misterhouse/1.0
-Cache-Control: no-cache
-
-EOT
-	stop $xbmc_osd;
+	my $get_xbmc_cmd = 'get_url \'http://' .$config_parms{xbmc_notify_address}.'/jsonrpc?request={"jsonrpc":"2.0","method":"GUI.ShowNotification","params":{"title":"'.$title.'","message":"'.$text.'"},"id":1}\' /dev/null';
+	my $p_xbmc = new Process_Item($get_xbmc_cmd);
+	start $p_xbmc;
 }
