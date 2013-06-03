61102b2e8536f1f3c04517368a902c07765a1923
Eloy Paris <peloy@chapus.net>
2013-06-03 12:26:53 -0400

    Fix issues with INSTEON lights in web interface
    INSTEON lights are treated differently in the web interface versus X10
    lights, for example: they get different icons, and cannot be dimmed or
    brightened. This patch does the following:
    
    - Use the same light icons for INSTEON lights that are used for X10
    lights.
    
    - Allow dimming/brightening of INSTEON lights by clicking on the left
    or right side of the icon in the web interface (only dimmable light, of
    course).
    
    - Fix a logic error that prevents disabling the button image cache. Ran
    into this while creating new icons for my web interface.
    
    - As an experimental "feature", and a small delay to try to give MH a
    chance to receive the new state of an INSTEON device changed through the
    web interface. This will allow the correct state to be displayed when
    the web page renders again. Disabled by default since it's just a hack
    (though it works good enough for me).
    
    Comments welcome.

--- web/bin/button_action.pl
+++ web/bin/button_action.pl
@@ -10,30 +10,62 @@ my ($list_name, $item, $state_xy) = @ARGV;
 my ($state, $x, $y) = $state_xy =~ /(\S+)\?(\d+),(\d+)/;
 #print "db ln=$list_name, i=$item, s=$state_xy xy=$x,$y\n";
 
-                                # Do not dim the dishwasher :)
-unless (eval qq|$item->isa('X10_Appliance') or $item->isa('Fan_Motor') or $item->isa('Insteon_Device')|) {
-    $state = 'dim'      if $x < 40;   # Left  side of image
-    $state = 'brighten' if $x > 110;  # Right side of image
-}
+my $object = &get_object_by_name($item);
 
-if (eval qq|$item->isa('EIB7_Item')|) { # Motor/drive states are stop/up/down
+if ($object->isa('X10_Item') && !$object->isa('X10_Appliance') ) {
+    # Do not dim the dishwasher :)
+
+    # Dim if clicked on left side of image, brighten if clicked on right
+    # side of image, or use state passed through the button URL if clicked
+    # in the center of the image.
+    if ($x < 40) {
+	$state = 'dim';
+    } elsif ($x > 110) {
+	$state = 'brighten';
+    }
+} elsif ($object->isa('EIB7_Item') ) { # Motor/drive states are stop/up/down
     $state = 'stop';
     $state = 'down' if $x < 40;         # Left  side of image
     $state = 'up' if $x > 110;          # Right side of image
+} elsif ($object->isa('Insteon::DimmableLight') ) {
+    my @states = $object->get_states();
+    my $curr_state = $object->state();
+
+    # Find the index into @states for the element that corresponds to the
+    # current state.
+    my ($index) = grep { $states[$_] eq $curr_state } 0..$#states;
+
+    # Dim if clicked on left side of image, brighten if clicked on right
+    # side of image, or use state passed through the button URL if clicked
+    # in the center of the image.
+    if ($x < 40) {
+	$index-- if ($index); # Can't dim if light is off
+	$state = $states[$index];
+    } elsif ($x > 110) {
+	$index++ if ($index != $#states); # Can't brighten if light is fully on
+	$state = $states[$index];
+    }
 }
 
-#if (eval qq|$item->isa('Insteon_Device'|) {
-#    $state = "toggle";
-#}
-
-eval qq|$item->set("$state", 'web')|;
-print "button_action.pl eval error: $@\n" if $@;
+$object->set("$state", 'web');
 
 #   print "dbx4a i=$item s=$state\n";
 #   my $object = &get_object_by_name($item);
 #   $state =   $$object{state};
 #   print "dbx4b i=$item s=$state\n";
 
+# Internal state of INSTEON devices does not change immediately after
+# clicking on the button. That is because, unlike X10 devices (for example),
+# an acknowledgement from the INSTEON device needs to be received so MH
+# can change the internal state. If we finish the HTTP transaction before
+# the acknowledge comes back then the resulting HTML page will display the
+# object that was just clicked on in the old state. This delay here prevents
+# this problem at the expense of, well, an extra delay. As this is
+# experimental, and this delay causes MH to pause for the duration of the
+# delay, this is currently disabled by default. But feel free to enable
+# to see if things improve.
+#sleep(1);
 
 my $h = &referer("/bin/list_buttons.pl?$list_name");
+
 return &http_redirect($h);
