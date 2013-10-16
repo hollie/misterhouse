de9e7d88d50e2aa5f8085dda8a7fb77a47665542
Pmatis <steve@switzerny.org>
2013-10-16 00:35:57 -0400

    Clean up lib/Audrey_Play.pm, add proper description.

--- lib/Audrey_Play.pm
+++ lib/Audrey_Play.pm
@@ -8,18 +8,22 @@ B<Audrey_Play> - This object can be used to play sound files on the Audrey.
 
 =head1 SYNOPSIS
 
-
-blah blah
-
-
+Created for use with PAobj.pm, but can be used separately.
 
 =head1 DESCRIPTION
+
+Tells an Audrey to download and play a file, already in the data/web folder,
+by passing the name of the file. The Audrey must be modified to respond to
+this request.
+
+$audrey1 = new Audrey_Play('192.168.0.11');
+#Create file data/web/tempfile.wav - perhaps by speaking to a file?
+my $speakFile = 'tempfile.wav';
+$audrey1->play($speakFile);
 
 =head1 INHERITS
 
 B<Generic_Item>
-
-=head1 METHODS
 
 =over
 
@@ -28,10 +32,6 @@ B<Generic_Item>
 @Audrey_Play::ISA = ('Generic_Item');
 
 my $address;
-
-sub Init {
-    #&::MainLoop_pre_add_hook(  \&Weather_Item::check_weather, 1 );
-}
 
 =item C<new($ip)>
 
@@ -63,3 +63,56 @@ sub play {
 }
 
 1;
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+=begin Audrey Config
+
+Exerpt from audreyspeak.pl
+
+You must make certain modifications to your Audrey, as follows:
+
+- Update the software and obtain root shell access capabilities (this
+  should be available by using Bruce's CF card image or by following
+  instructions available on the internet.)
+
+- Open the Audrey's web server to outside http requests
+  1) Start the "Root Shell"
+  2) type: cd /config
+  3) type: cp rm-apps rm-apps.copy
+  4) type: vi rm-apps
+     You'll be in the editor, editing the "rm-apps" file
+     About the 14th line down is "rb,/kojak/kojak-slinger, -c -e -s -i 127.1"
+     You need to delete the "-i 127.1" from the line.
+     To do this, place the cursor under the space right after the "-s"
+     Type the "x" key to start deleting from the line.
+     The line should end up looking like this:
+     "rb,/kojak/kojak-slinger, -c -e -s"
+     If you need to start over type a colon to get to the vi command line
+     At the colon prompt type "q!" and hit "enter" (this quits without saving)
+     If it looks good then at the colon prompt type "wq" to save changes
+     Now restart the Audrey by unplugging it, waiting 30 seconds and
+     plugging it back in.
+
+- Install playsound_noph and it's DLL
+  1) Grab the zip file from http://www.planetwebb.com/audrey/
+  2) Place playsound_noph    on the Audrey in /nto/photon/bin/
+  3) Place soundfile_noph.so on the Audrey in /nto/photon/dll/
+
+- Install mhspeak.shtml on the Audrey
+  1) Start the "Root Shell"
+  2) type: cd /data/XML
+  3) type: ftp blah.com mhspeak.shtml
+
+     The MHSPEAK.SHTML file placed on the Audrey should contain the following:
+
+     <html>
+     <head>
+     <title>Shell</title>
+     </head>
+     <body>
+     <!--#config cmdecho="OFF" -->
+     <!--#exec cmd="playsound_noph $QUERY_STRING &" -->
+     </body>
+     </html>
+
+=cut
