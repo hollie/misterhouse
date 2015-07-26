46d018919ed2a287aacf6605d74d795ef687f620
H Plato <hplato@gmail.com>
2015-07-25 20:28:18 -0600

    Fixed a typo

--- code/common/opensprinkler_external.pl
+++ code/common/opensprinkler_external.pl
@@ -2,14 +2,14 @@
 
 #@Utility to run an external script and parse the result into updated runtimes
 
-my $os_external_source 			= "";
-$os_external_source 			= $config_parms{os_external_data_source} if (defined $config_parms{os_external_data_source}) ;
+my $os_external_source 		= "";
+$os_external_source 		= $config_parms{os_external_data_source} if (defined $config_parms{os_external_data_source}) ;
 my $os_external_output_file 	= "";
-$os_external_output_file 		= $config_parms{os_external_data_output_file} if (defined $config_parms{os_external_data_output_file}) ;
+$os_external_output_file 	= $config_parms{os_external_data_output_file} if (defined $config_parms{os_external_data_output_file}) ;
 my $os_external_program_crontab = "";
 $os_external_program_crontab 	= $config_parms{os_external_data_crontab} if (defined $config_parms{os_external_data_crontab}) ;
-my $os_program					= "";
-$os_program 					= $config_parms{os_external_data_program} if (defined $config_parms{os_external_data_program) ;
+my $os_program			= "";
+$os_program 			= $config_parms{os_external_data_program} if (defined $config_parms{os_external_data_program}) ;
 
 
 my $start;
@@ -17,7 +17,7 @@ my $start;
 $p_os_extprog = new Process_Item($os_external_source);
 eval ($start = time_cron($os_external_program_crontab));
 
-if ($start) {
+if ($start or $Startup) {
 	print_log "[OpenSprinkler] Get_ext_data: Starting external program...";
 	start $p_os_extprog;
 }
