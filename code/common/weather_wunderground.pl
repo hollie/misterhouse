2cd2349d13fa76b6c5336107bbf840dc0820e255
Pmatis <steve@switzerny.org>
2016-09-18 13:41:09 -0400

    Updating to version that I'm using, cleaned up code, add api_key detection.

--- code/common/weather_wunderground.pl
+++ code/common/weather_wunderground.pl
@@ -1,8 +1,7 @@
 # Category = Weather
-
 #@ Updates live weather variables from http://api.wunderground.com. 
 
-=begin comment
+=begin
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 File:
@@ -29,165 +28,126 @@ Special Thanks to:
 
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 =cut
+
 # noloop=start
 	use Weather_Common;
+	use Data::Dumper;
 	use XML::Twig;
-	my $wunderground_file_getweather;
-	my $wunderground_file_getforecast;
-	$p_weather_wunderground_getweather	= new Process_Item();
-	$p_weather_wunderground_getforecast	= new Process_Item();
+	my $wunderground_getweather_file;
+	my $wunderground_stationid=$config_parms{wunderground_stationid};
+	my $wunderground_apikey=$config_parms{wunderground_apikey};
+	my $wunderground_url;
+	my %wunderground_data;
+	my @wunderground_keys;
+	$p_weather_wunderground_getweather =  new Process_Item();
 	
-	my $wunderground_states = 'getweather,getforecast,debug';
+	my $wunderground_states = 'getweather,parsefile,debug';
 	$v_wunderground = new Voice_Cmd("wunderground [$wunderground_states]");
 # noloop=stop
 
 if ($Reload) {
-	$wunderground_file_getweather=$config_parms{data_dir}.'/web/weather_wunderground_weather.xml';
-	$wunderground_file_getforecast=$config_parms{data_dir}.'/web/weather_wunderground_forecast.xml';
-	if($config_parms{wunderground_stationid} eq '') {
-		print_log("Warning: wunderground_stationid is not defined in mh.private.ini.");
-	}
+	$wunderground_stationid=$config_parms{wunderground_stationid};
+	$wunderground_apikey=$config_parms{wunderground_apikey};
+	$wunderground_stationid='' unless $wunderground_stationid;
+	$wunderground_getweather_file=$config_parms{data_dir}.'/web/weather_wunderground_getweather.xml';
 	if($config_parms{wunderground_apikey} eq '') {
-		print_log("Warning: wunderground_apikey is not defined in mh.private.ini.");
-	}
-	if($config_parms{state} eq '') {
-		print_log("Warning: state is not defined in mh.private.ini.");
-	}
-	if($config_parms{wunderground_city} eq '') {
-		print_log("Warning: wunderground_city is not defined in mh.private.ini.");
-	}
+		print_log("[WUnderground] ERROR: wunderground_apikey is not defined in mh.private.ini.");
+	} else {
+		if($wunderground_stationid ne '') {
+			print_log "[WUnderground] Using Weather underground StationID: $wunderground_stationid" if $Debug{weather};
+			$wunderground_url='http://api.wunderground.com/api/'.$wunderground_apikey.'/conditions/q/pws:'.$wunderground_stationid.'.xml';
+		} elsif($config_parms{zip_code} ne '') {
+			print_log "[WUnderground] Using ZIP Code: " . $config_parms{zip_code} if $Debug{weather};
+			$wunderground_url='http://api.wunderground.com/api/'.$wunderground_apikey.'/conditions/q/'.$config_parms{zip_code}.'.xml';
+		} else {
+			print_log "[WUnderground] WARNING: wunderground_stationid and zip_core are both blank in mh.private.ini. Leaving it to Weather underground to guess your location by IP address..." if $Debug{weather};
+			$wunderground_url='http://api.wunderground.com/api/'.$wunderground_apikey.'/conditions/q/autoip.xml';
+		}
+
+		print_log "[WUnderground] Using URL: '".$wunderground_url if $Debug{weather};
 	
-	#$p_weather_wunderground_getweather-set_output("$wunderground_file_getweather");
-	
-	&trigger_set(qq|time_cron('*/15 * * * *') or \$Reload|, "run_voice_cmd 'wunderground getweather'", 'NoExpire', 'Update current weather conditions via wunderground')
-	  unless &trigger_get('Update current weather conditions via wunderground');
-	&trigger_set('($New_Minute && $Minute == 5) or $Reload', "run_voice_cmd 'wunderground getforecast'", 'NoExpire', 'Update forecast via wunderground')
-	  unless &trigger_get('Update forecast via wunderground');
+		&trigger_set(qq|time_cron('*/15 * * * *') or \$Reload|, "run_voice_cmd 'wunderground getweather'", 'NoExpire', 'Update current weather conditions via wunderground')
+		  unless &trigger_get('Update current weather conditions via wunderground');
+	}
 }
 
 my $wunderground_state = 'blank';
-if ($wunderground_state = $v_wunderground->{said}) {
-	if ($wunderground_state eq 'debug'){
-		#Not implemented
-	} elsif ($wunderground_state eq 'getweather'){
-		if($config_parms{wunderground_stationid} eq '') {
-			print_log("ERROR: wunderground_stationid is not defined in mh.private.ini.");
-		} else {
-			my $wunderground_url='http://api.wunderground.com/weatherstation/WXCurrentObXML.asp?ID='.$config_parms{wunderground_stationid};
-			print_log('Retrieving: '.$wunderground_url);
-			set $p_weather_wunderground_getweather qq{get_url -quiet "$wunderground_url" "$wunderground_file_getweather"};
-			start $p_weather_wunderground_getweather;
-		}
-	} elsif ($wunderground_state eq 'getforecast'){
-		my $wundergrounderror=0;
-		if($config_parms{wunderground_apikey} eq '') {
-			print_log("ERROR: wunderground_apikey is not defined in mh.private.ini.");
-			$wundergrounderror++;
-		}
-		if($config_parms{state} eq '') {
-			print_log("ERROR: state is not defined in mh.private.ini.");
-			$wundergrounderror++;
-		}
-		if($config_parms{wunderground_city} eq '') {
-			print_log("ERROR: wunderground_city is not defined in mh.private.ini.");
-			$wundergrounderror++;
-		}
-		if ($wundergrounderror == 0) {
-			my $wustate=uc $config_parms{state};
-			my $wucity=$config_parms{wunderground_city};
-			my $wunderground_url='https://api.wunderground.com/api/'.$config_parms{wunderground_apikey}.'/forecast/q/'.$wustate.'/'.$wucity.'.xml';
-			print_log('Retrieving: '.$wunderground_url);
-			set $p_weather_wunderground_getforecast qq{get_url -quiet "$wunderground_url" "$wunderground_file_getforecast"};
-			start $p_weather_wunderground_getforecast;
-		}
+if ($wunderground_state = $v_wunderground ->{said}) {
+	if ($wunderground_state eq 'getweather'){
+		print_log "[WUnderground] Getting data from $wunderground_url" if $Debug{weather};
+		set $p_weather_wunderground_getweather qq{get_url -quiet "$wunderground_url" "$wunderground_getweather_file"};
+		start $p_weather_wunderground_getweather;
 	}
 }
 
-my(%wunderground_data,@wunderground_keys);
-
-if (done_now $p_weather_wunderground_getweather) {
+if (done_now $p_weather_wunderground_getweather or 'parsefile' eq said $v_wunderground) {
 	$Weather{wunderground_obsv_valid} = 0; #Set to not valid unless proven
-  	#my $wunderground_xml=file_read $wunderground_file_getweather;
-	print_log "wunderground getweather finished.";
-	my $twig = new XML::Twig;
-	$twig->parsefile($wunderground_file_getweather);
+  	#my $wunderground_xml=file_read $wunderground_getweather_file;
+	print_log "[WUnderground] getweather finished, data written to $wunderground_getweather_file" if $Debug{weather};
+	my $twig = new XML::Twig();
+  	$twig->parsefile($wunderground_getweather_file);
+	$twig->print if $Debug{weather} >= 5;
 	my $root = $twig->root;
 	my $channel = $root->first_child("current_observation");
+
+	print_log "[WUnderground] " . Dumper $channel if $Debug{weather} >= 5;
 	
 	my $w_stationid = $root->first_child_text("station_id");
 	
-	print_log "WUnderground: Received stationid: $w_stationid" if $Debug{wunderground};
-	
+	print_log "[WUnderground] Received stationid: $w_stationid" if $Debug{weather};
+
+	$config_parms{wunderground_stationid} = $w_stationid;
+
 	if($config_parms{wunderground_stationid} eq $w_stationid) {
 		%wunderground_data={};
 		@wunderground_keys=[];
 		#TempOutdoor
-		weather_wunderground_addelem($root,'TempOutdoor','temp_f');
+		weather_wunderground_addelem($channel,'TempOutdoor','temp_f');
 		#DewOutdoor
-		weather_wunderground_addelem($root,'DewOutdoor','dewpoint_f');
+		weather_wunderground_addelem($channel,'DewOutdoor','dewpoint_f');
 		#WindAvgDir
-		weather_wunderground_addelem($root,'WindAvgDir','wind_dir');
+		weather_wunderground_addelem($channel,'WindAvgDir','wind_dir');
 		#WindAvgSpeed
-		weather_wunderground_addelem($root,'WindAvgSpeed','wind_mph');
+		weather_wunderground_addelem($channel,'WindAvgSpeed','wind_mph');
 		#WindGustDir
 		#WindGustSpeed
+		weather_wunderground_addelem($channel,'WindGustSpeed','wind_gust_mph');
 		#WindGustTime
 		#Clouds
 		#Conditions
+		weather_wunderground_addelem($channel,'Conditions','weather');
 		#Barom
-		weather_wunderground_addelem($root,'Barom','pressure_mb');
+		weather_wunderground_addelem($channel,'Barom','pressure_mb');
 		#BaromSea
 		#BaromDelta
 		#HumidOutdoorMeasured
 		#HumidOutdoor
-		weather_wunderground_addelem($root,'HumidOutdoor','relative_humidity');
+		weather_wunderground_addelem($channel,'HumidOutdoor','relative_humidity');
 		#IsRaining
 		#IsSnowing
 		#RainTotal
+		weather_wunderground_addelem($channel,'RainTotal','precip_today_in');
 		#RainRate
 		
-		#use Data::Dumper;
-		#print Dumper %wunderground_data;
+		print_log "[WUnderground] " . Dumper %wunderground_data if $Debug{weather} >= 5;
 		
+		print_log "[WUnderground] Using elements: $config_parms{weather_wunderground_elements}" if $Debug{weather};
 		&Weather_Common::populate_internet_weather(\%wunderground_data, $config_parms{weather_wunderground_elements});
 		&Weather_Common::weather_updated;
-		      
+		
 	} else {
-		print_log "WUnderground: ERROR! Received a station ID we didn't want. Aborting.";
+		print_log "[WUnderground] ERROR! Received a station ID we didn't want. Aborting.";
 	}
 	
-}
-
-if (done_now $p_weather_wunderground_getforecast) {
-	$Weather{wunderground_obsv_valid} = 0; #Set to not valid unless proven
-  	#my $wunderground_xml=file_read $wunderground_file_getweather;
-	print_log "wunderground getforecast finished.";
-	my $twig = new XML::Twig;
-	print_log('Reading file: '.$wunderground_file_getforecast);
-	$twig->parsefile($wunderground_file_getforecast);
-	my $root = $twig->root;
-	my $fcast = $root->first_child("forecast");
-	my $txtfcast = $fcast->first_child("txt_forecast");
-	my $fcdays = $txtfcast->first_child('forecastdays');
-	
-	my @forecast = $fcdays->children('forecastdays');
-	
-	print "\n\n";
-	print $fcdays->text();
-	print "\n\n";
-
-	foreach my $fcday (@forecast) {
-		print_log($fcday->first_child_text('fcttext'));
-	}
-	
-	use Data::Dumper;
-	#print Dumper @forecast;
 }
 
 sub weather_wunderground_addelem {
-	my ($w_root,$w_dest,$w_src) = @_;
-	if(my $w_srcval=$w_root->first_child_text("$w_src")) {
-		print_log sprintf("WUnderground: Data: %15s = %8s (%s)",$w_dest,$w_srcval,$w_src) if $Debug{wunderground};
+	my ($w_twigroot,$w_dest,$w_src) = @_;
+	print_log "[WUnderground] Looking for $w_src" if $Debug{weather};
+
+	if(my $w_srcval=$w_twigroot->first_child_text("$w_src")) {
+		$w_srcval =~ s/%$//;
+		print_log sprintf("WUnderground: Data: %15s = %8s (%s)",$w_dest,$w_srcval,$w_src) if $Debug{weather};
 		$wunderground_data{$w_dest}=$w_srcval;
 		push(@wunderground_keys, $w_dest);
 	}
