b37e543dd61185eac10ae4c9e5a764ba90fea5bf
waynieack <wayne@razorcla.ws>
2020-08-13 20:48:38 -0500

    Fixed and completed the Insteon Thermostat functionality

--- lib/AoGSmartHome_Items.pm
+++ lib/AoGSmartHome_Items.pm
@@ -450,9 +450,9 @@ EOF
 	    # the device has the "Brightness" trait. Other types of dimmable
 	    # objects will have to be added here (right now we only check for
 	    # INSTEON and X10 dimmable lights).
+	    &main::print_log("[AoGSmartHome] Object Name: ". $self->{'uuids'}->{$uuid}->{'realname'} ."\n");
 	    my $mh_object = ::get_object_by_name($self->{'uuids'}->{$uuid}->{'realname'});
-	    if ($mh_object->isa('Insteon::DimmableLight') 
-		|| $mh_object->can('state_level') ) {
+	    if ( (defined $mh_object) && ($mh_object->isa('Insteon::DimmableLight') || $mh_object->can('state_level')) ) {
 		$response .= <<EOF;
      "action.devices.traits.Brightness",
 EOF
@@ -513,8 +513,7 @@ EOF
 	    $response .= <<EOF;
    },
 EOF
-        }
-	elsif ( $type eq 'thermostat') {
+        } elsif ( $type eq 'thermostat') {
 	    my $mh_object = ::get_object_by_name($self->{'uuids'}->{$uuid}->{'realname'});
 	    if (!$mh_object->isa('Insteon::Thermostat') ) {
 		&main::print_log("[AoGSmartHome] '$self->{'uuids'}->{$uuid}->{'realname'} is an unsupported thermostat; ignoring AoG item.");
@@ -531,9 +530,9 @@ EOF
     "name": {
      "name": "$self->{'uuids'}->{$uuid}->{'name'}"
     },
-    "willReportState": false,
+    "willReportState": true,
     "attributes": {
-     "availableThermostatModes": "off,heat,cool,on",
+     "availableThermostatModes": "off,heat,cool,on,heatcool,fan-only",
      "thermostatTemperatureUnit": "F"
     },
 EOF
@@ -626,23 +625,43 @@ EOF
         elsif ( $self->{'uuids'}->{$uuid}->{'type'} eq 'thermostat' ) {
 	    my $mh_object = ::get_object_by_name($self->{'uuids'}->{$uuid}->{'realname'});
 	    if ($mh_object->isa('Insteon::Thermostat') ) {
-		my $mode = $mh_object->get_mode();
+		my $mode = lc($mh_object->get_mode());
+		$mode = 'heatcool' if ($mode =~ /auto/);
+
+		my $activeThermostatMode = lc($mh_object->get_status);
+		my $fanmode = lc($mh_object->get_fan_mode);
+		if ($activeThermostatMode =~ /cooling/) { 
+			$activeThermostatMode = 'cool';
+		} elsif ($activeThermostatMode =~ /heating/) { 
+			$activeThermostatMode = 'heat';
+		} elsif ( ( $fanmode =~ /always on/) and ( $activeThermostatMode =~ /off/ ) ) {
+			$activeThermostatMode = 'fan-only';
+		} else { 
+			$activeThermostatMode = 'none';
+		}
 
 		my $temp_setpoint;
 		if ($mode eq 'cool') {
-		    $temp_setpoint = $mh_object->get_cool_sp();
-		} else {
-		    $temp_setpoint = $mh_object->get_heat_sp();
+		    $temp_setpoint = '"thermostatTemperatureSetpoint": '. &FtoC($mh_object->get_cool_sp).',';
+		} elsif ($mode eq 'heat') {
+		    $temp_setpoint = '"thermostatTemperatureSetpoint": '. &FtoC($mh_object->get_heat_sp).',';
+		} elsif ($mode eq 'heatcool') {
+		     $temp_setpoint = '"thermostatTemperatureSetpointHigh": '. &FtoC($mh_object->get_cool_sp).','."\n";
+		     $temp_setpoint .= '"thermostatTemperatureSetpointLow": '. &FtoC($mh_object->get_heat_sp).',';
 		}
-
-		my $temp_ambient = $mh_object->get_temp();
-
+		
+		my $temp_ambient = &FtoC($mh_object->get_temp);
+		my $thermostatHumidityAmbient = $mh_object->get_humid;
+		
 		$response .= <<EOF;
    "$uuid": {
     "online": true,
     "thermostatMode": "$mode",
-    "thermostatTemperatureSetpoint": "$temp_setpoint",
-    "thermostatTemperatureAmbient": "$temp_ambient",
+    "activeThermostatMode": "$activeThermostatMode",
+    $temp_setpoint
+    "thermostatTemperatureAmbient": $temp_ambient,
+    "thermostatHumidityAmbient": $thermostatHumidityAmbient,
+    "status": "SUCCESS"
    },
 EOF
 	    }
@@ -651,7 +670,6 @@ EOF
 
 	    next;
 	}
-
         #
         # The device is a light, a switch, or an outlet.
         #
@@ -720,6 +738,19 @@ EOF
     }
 
     return &main::json_page($response);
+}
+
+sub FtoC { 
+	my ( $F ) = @_;
+	return ( ($F - 32) * 5/9 );
+	#return sprintf "%.0f", ( ($F - 32) * 5/9 );
+}
+
+sub CtoF { 
+	my ( $F ) = @_;
+	#return ( (9 * $F/5) + 32 );
+	return sprintf "%.0f", ( (9 * $F/5) + 32 );
+
 }
 
 sub execute_OnOff {
@@ -812,28 +843,53 @@ sub execute_ThermostatX {
         return undef if !defined $mh_object;
 
 	if ($mh_object->isa('Insteon::Thermostat') ) {
+	    my $mode = lc($mh_object->get_mode);
+	    $mode = 'heatcool' if ($mode =~ /auto/);
+
 	    if ( $execution_command =~ /TemperatureSetpoint/ ) {
-		my $setpoint = $command->{'execution'}->[0]->{'params'}->{'thermostatTemperatureSetpoint'};
-		if ($mh_object->get_mode() eq 'cool') {
+		&main::print_log("[AoGSmartHome] Setting temp: ".&CtoF($command->{'execution'}->[0]->{'params'}->{'thermostatTemperatureSetpoint'}) ) if ( $main::Debug{'aog'} );
+		my $setpoint = &CtoF($command->{'execution'}->[0]->{'params'}->{'thermostatTemperatureSetpoint'});
+		if ( $mode eq 'cool') {
 		    $mh_object->cool_setpoint($setpoint);
-		} else {
+		} elsif( $mode eq 'heat') {
 		    $mh_object->heat_setpoint($setpoint);
 		}
+	    } elsif ( $execution_command =~ /ThermostatTemperatureSetRange/ ) {
+		&main::print_log("[AoGSmartHome] Setting cool: ".&CtoF($command->{'execution'}->[0]->{'params'}->{'thermostatTemperatureSetpointHigh'})." Heat: ".&CtoF($command->{'execution'}->[0]->{'params'}->{'thermostatTemperatureSetpointLow'})) if ( $main::Debug{'aog'} );
+		$mh_object->cool_setpoint( &CtoF($command->{'execution'}->[0]->{'params'}->{'thermostatTemperatureSetpointHigh'}) );
+		$mh_object->heat_setpoint( &CtoF($command->{'execution'}->[0]->{'params'}->{'thermostatTemperatureSetpointLow'}) );
 	    } elsif ( $execution_command =~ /ThermostatSetMode/ ) {
 		my $mode = $command->{'execution'}->[0]->{'params'}->{'thermostatMode'};
+		$mode = 'auto' if ($mode =~ /heatcool/);
 		$mh_object->mode($mode);
 	    }
 
-	    my $mode = $mh_object->get_mode();
+	    $mode = lc($mh_object->get_mode);
+	    $mode = 'heatcool' if ($mode =~ /auto/);
+	    my $activeThermostatMode = lc($mh_object->get_status());
+	    my $fanmode = lc($mh_object->get_fan_mode);
+	    if ($activeThermostatMode =~ /cooling/) {
+            	$activeThermostatMode = 'cool';
+	    } elsif ($activeThermostatMode =~ /heating/) {
+            	$activeThermostatMode = 'heat';
+	    } elsif ( ( $fanmode =~ /always on/) and ( $activeThermostatMode =~ /off/ ) ) {
+            	$activeThermostatMode = 'fan-only';
+            } else {
+            	$activeThermostatMode = 'none';
+            }
 
-	    my $temp_setpoint;
-	    if ($mode eq 'cool') {
-		$temp_setpoint = $mh_object->get_cool_sp();
-	    } else {
-		$temp_setpoint = $mh_object->get_heat_sp();
-	    }
+            my $temp_setpoint;
+            if ($mode eq 'cool') {
+            	$temp_setpoint = '"thermostatTemperatureSetpoint": '. &FtoC($mh_object->get_cool_sp).',';
+            } elsif ($mode eq 'heat') {
+            	$temp_setpoint = '"thermostatTemperatureSetpoint": '. &FtoC($mh_object->get_heat_sp).',';
+            } elsif ($mode eq 'heatcool') {
+            	$temp_setpoint = '"thermostatTemperatureSetpointHigh": '. &FtoC($mh_object->get_cool_sp).','."\n";
+            	$temp_setpoint .= '"thermostatTemperatureSetpointLow": '. &FtoC($mh_object->get_heat_sp).',';
+            }  
 
-	    my $temp_ambient = $mh_object->get_temp();
+	    my $temp_ambient = &FtoC($mh_object->get_temp);
+	    my $thermostatHumidityAmbient = $mh_object->get_humid;
 
 	    $response .= "   {";
 	    $response .= <<EOF
@@ -841,8 +897,10 @@ sub execute_ThermostatX {
     "status": "SUCCESS",
     "states": {
      "thermostatMode": "$mode",
-     "thermostatTemperatureSetpoint": "$temp_setpoint",
-     "thermostatTemperatureAmbient": "$temp_ambient",
+     "activeThermostatMode": $activeThermostatMode,
+     $temp_setpoint
+     "thermostatTemperatureAmbient": $temp_ambient,
+     "thermostatHumidityAmbient": $thermostatHumidityAmbient
     }
   },
 EOF
